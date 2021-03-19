13bc3f371078f3ae0b67fe48bf222fa14219b00f
relogu <lollonasi97@gmail.com>
2021-03-19 10:58:45 +0100

    initial commit

---
+++ flower_cross_mk_moons_test/README.md
@@ -0,0 +1,28 @@
+# Test using FLOWER by Adap and sklearn.make_moons with cross-validation
+This test provides a client and a server program with minimal configuration
+
+## Usage
+Firstly set the server machine and launch the server program.
+```bash
+python3 server.py
+```
+Parameters to pass are explain at
+```bash
+
+```
+Firstly set a series of client machines and launch the client program for each machine.
+```bash
+python3 client.py
+```
+Parameters to pass are explain at
+```bash
+
+```
+
+## Results
+
+
+
+## References
+
+<blockquote>1- Beutel, Daniel J and Topal, Taner and Mathur, Akhil and Qiu, Xinchi and Parcollet, Titouan and Lane, Nicholas D Flower: A Friendly Federated Learning Research Framework. arXiv preprint arXiv:2007.14390, 2020. </blockquote>
---
+++ flower_cross_mk_moons_test/client.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 14:25:15 2021
+
+@author: relogu
+"""
+import argparse
+import os
+import flwr as fl
+import tensorflow as tf
+from sklearn import datasets
+from sklearn.model_selection import KFold
+from argparse import RawTextHelpFormatter
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('Agg')
+import random
+# disable possible gpu devices
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+# Make TensorFlow log less verbose
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+# for debug connection
+os.environ["GRPC_VERBOSITY"] = "debug"
+
+def parse_args():
+    """Parse the arguments passed."""
+    description = 'Client for moons test FL network using flower.\n' + \
+        'Give the id of the client and the number of local epoch you want to perform.\n' + \
+        'Give also the number of data samples you wanto to train for this client.\n' + \
+        'One can optionally give the server location to pass the client builder.\n' + \
+        'One can optionally give the noise to generate the dataset.\n' + \
+        'One can optionally tell the program to plot the decision boundary at the evaluation step.\n' + \
+        'The number of test data samples is fixed by the program.\n' + \
+        'The client id will also initialize the seed for the train dataset.\n' + \
+        'The program is built to make all the client use the same test dataset.'
+    parser = argparse.ArgumentParser(description = description,
+                                     formatter_class=RawTextHelpFormatter)
+    parser.add_argument('--server',
+                        dest='server',
+                        required=False,
+                        type=type(''),
+                        action='store',
+                        help='server address to point')
+    parser.add_argument('--client_id',
+                        dest='client_id',
+                        required=True,
+                        type=int,
+                        action='store',
+                        help='client id, set also the seed for the dataset')
+    parser.add_argument('--rounds',
+                        dest='rounds',
+                        required=False,
+                        type=int,
+                        action='store',
+                        help='number of local epochs to perform')
+    parser.add_argument('--n_samples',
+                        dest='n_samples',
+                        required=True,
+                        type=int,
+                        action='store',
+                        help='number of total samples')
+    parser.add_argument('--n_folds',
+                        dest='n_folds',
+                        required=False,
+                        type=int,
+                        action='store',
+                        help='number of folds for the cross validator')
+    parser.add_argument('--plot',
+                        dest='plot',
+                        required=False,
+                        type=bool,
+                        action='store',
+                        help='tells the program whether to plot decision boundary or not')
+    _args = parser.parse_args()
+    return _args
+
+
+def plot_decision_boundary(model, fed_iter, x, y):
+    """Plot the decision boundary given the predictions of the model."""
+    plt.figure(figsize=(18, 9))
+    ax = plt.subplot(1, 1, 1)
+    ax.set_title("Decision boundary for the test set at the federated round: " + str(fed_iter))
+    # Set min and max values and give it some padding
+    x_min, x_max = x[:, 0].min() - .5, x[:, 0].max() + .5
+    y_min, y_max = x[:, 1].min() - .5, x[:, 1].max() + .5
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole gid
+    Z = np.argmax(model.predict(np.c_[xx.ravel(), yy.ravel()]), axis=-1)
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.scatter(x[:, 0], x[:, 1], c=y, cmap=plt.cm.Spectral)
+    plt.draw()
+    #plt.show(block=False)
+    plt.savefig('output/dec_bound_F'+str(fed_iter)+'.png')
+    plt.close()
+
+if __name__ == "__main__":
+
+    #parsing arguments
+    args = parse_args()
+
+    # Load and compile Keras model
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Input((2,)),
+        tf.keras.layers.Dense(4, activation='tanh',
+                              kernel_initializer=tf.keras.initializers.GlorotUniform(),
+                              bias_initializer='ones',),
+        tf.keras.layers.Dense(2, activation='softmax',
+                              kernel_initializer=tf.keras.initializers.GlorotUniform(),
+                              bias_initializer='ones',)])
+    model.compile("adam",
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=["accuracy"])
+
+    # parameters
+    N_SAMPLES = int(args.n_samples)
+    if N_SAMPLES < 10:
+        N_SAMPLES=10
+    if N_SAMPLES < 10:
+        N_SAMPLES=10
+        
+    if not args.n_folds:
+        N_FOLDS = 2
+    else :
+        N_FOLDS = int(args.n_folds)
+        
+    if not args.rounds:
+        N_LOC_EPOCHS = 1
+    else:
+        N_LOC_EPOCHS = args.rounds
+    if N_LOC_EPOCHS < 1:
+        N_LOC_EPOCHS = 1
+        
+    NOISE = 0.2
+    
+    if not args.plot:
+        PLOT = False
+    else:
+        PLOT = args.plot
+        
+    random.seed(args.client_id)
+    RAND_STATE = random.randint(0, 100000)
+
+    # datasets
+    (x, y) = datasets.make_moons(n_samples=N_SAMPLES,
+                                 shuffle=True,
+                                 noise=NOISE,
+                                 random_state=RAND_STATE)
+    
+    # Define the K-fold Cross Validator
+    kfold = KFold(n_splits=N_FOLDS, shuffle=True) #shuffling may be useless
+    n_splits  = kfold.get_n_splits(x, y)
+    
+    def get_train_set(f_round):
+        """Return the index for the train dataset, given the federated iteration."""
+        index = (f_round-1)%n_splits
+        i=0
+        for train, _ in kfold.split(x, y):
+            if i==index :
+                return train
+            i+=1
+    
+    def get_test_set(f_round):
+        """Return the index for the test dataset, given the federated iteration."""
+        index = (f_round-1)%n_splits
+        i=0
+        for _, test in kfold.split(x, y):
+            if i==index :
+                return test
+            i+=1
+
+    class MakeMoonsClient(fl.client.NumPyClient):
+        """Client object, to set client performed operations."""
+        
+        def __init__( self ):
+            self.f_round = 0
+        
+        def get_parameters(self):  # type: ignore
+            """Get the model weights by model object."""
+            return model.get_weights()
+
+        def fit(self, parameters, config):  # type: ignore
+            """Perform the fit step after having assigned new weights."""
+            self.f_round += 1
+            if self.f_round%10 == 0 :
+                print("Federated Round number " + str(self.f_round))
+            x_train = x[get_train_set(self.f_round)]
+            y_train = y[get_train_set(self.f_round)]
+            model.set_weights(parameters)
+            model.fit(x_train, y_train, epochs=N_LOC_EPOCHS, verbose=0)#, batch_size=32)
+            return model.get_weights(), len(x_train), {}
+
+        def evaluate(self, parameters, config):  # type: ignore
+            """Perform the evaluation step after having assigned new weights."""
+            model.set_weights(parameters)
+            x_test = x[get_test_set(self.f_round)]
+            y_test = y[get_test_set(self.f_round)]
+            if self.f_round%100 == 0 and PLOT:
+                loss, accuracy = model.evaluate(x_test, y_test, verbose=1)
+                plot_decision_boundary(model, self.f_round, x_test, y_test)
+            else :
+                loss, accuracy = model.evaluate(x_test, y_test, verbose=0)
+            return loss, len(x_test), {"accuracy": accuracy}
+
+    if not args.server:
+        SERVER = "192.168.1.191:5223"
+    else:
+        SERVER = args.server
+    print(SERVER)
+    # Start Flower client
+    fl.client.start_numpy_client(SERVER, client=MakeMoonsClient())
