c54dd7a852be65ce05e49dcc21591c6fd33ed4c4
relogu <lollonasi97@gmail.com>
2021-03-30 10:14:48 +0200

    correct to plot all the l curves of the results

--- flower_make_moons_test/plot_learning_curves.py
+++ flower_make_moons_test/plot_learning_curves.py
@@ -14,22 +14,25 @@ import glob
 from mpl_toolkits.mplot3d import Axes3D
 import pathlib
 
-path = '/home/relogu/Desktop/OneDrive/UNIBO/Magistrale/Federated Learning Project/flower_make_moons_test'
+path = '/home/relogu/Desktop/OneDrive/UNIBO/Magistrale/Federated Learning Project/RESULTS/'
+folders =  glob.glob(path+'*')
+for folder in folders:
+    files = glob.glob(folder+'/*.dat')
+    conv = pd.read_csv(files[0], index_col=False)
+    for file in files[1:]:
+        conv = conv.append(pd.read_csv(file, index_col=False), ignore_index = True)
+        
+    sns.set_style('whitegrid')
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10,5))
+    plt.title(file)
+    ax.set_ylabel('accuracy')
+    plt.xlabel("round")
+    sns.lineplot(x='round', y='accuracy', hue='client', data=conv, style='client')#, markers=['.', '.'])
+    plt.show()
 
-files = glob.glob(path+'/output/*.dat')
-
-conv = pd.read_csv(files[0], index_col=False)
-for file in files[1:]:
-    conv = conv.append(pd.read_csv(file, index_col=False), ignore_index = True)
-
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10,5))
-ax.set_ylabel('accuracy')
-plt.xlabel("round")
-sns.scatterplot(x='round', y='accuracy', hue='client', data=conv)
-plt.show()
-
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10,5))
-ax.set_ylabel('loss')
-plt.xlabel("round")
-sns.scatterplot(x='round', y='loss', hue='client', data=conv)
-plt.show()
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10,5))
+    plt.title(file)
+    ax.set_ylabel('loss')
+    plt.xlabel("round")
+    sns.lineplot(x='round', y='loss', hue='client', data=conv, style='client')#, markers=['.', '.'])
+    plt.show()
