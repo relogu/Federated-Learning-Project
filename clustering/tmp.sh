de76f3b527d2122f345a8149e87d46334985e51c
relogu <lollonasi97@gmail.com>
2021-06-18 13:55:57 +0200

    script for launching training

---
+++ clustering/tmp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# clustergan and lda partitions
+python3 py/server.py --strategy=clustergan --total_epochs=100 --n_clients=8 & 
+sleep 2 # Sleep for 2s to give the server enough time to start
+python3 py/client.py --client_id=0 --n_samples=70000 --alg=clustergan --n_clients=8 --n_clusters=10 --lda=True --dataset=mnist &
+python3 py/client.py --client_id=1 --n_samples=70000 --alg=clustergan --n_clients=8 --n_clusters=10 --lda=True --dataset=mnist &
+python3 py/client.py --client_id=2 --n_samples=70000 --alg=clustergan --n_clients=8 --n_clusters=10 --lda=True --dataset=mnist &
+python3 py/client.py --client_id=3 --n_samples=70000 --alg=clustergan --n_clients=8 --n_clusters=10 --lda=True --dataset=mnist &
+python3 py/client.py --client_id=4 --n_samples=70000 --alg=clustergan --n_clients=8 --n_clusters=10 --lda=True --dataset=mnist &
+python3 py/client.py --client_id=5 --n_samples=70000 --alg=clustergan --n_clients=8 --n_clusters=10 --lda=True --dataset=mnist &
+python3 py/client.py --client_id=6 --n_samples=70000 --alg=clustergan --n_clients=8 --n_clusters=10 --lda=True --dataset=mnist &
+python3 py/client.py --client_id=7 --n_samples=70000 --alg=clustergan --n_clients=8 --n_clusters=10 --lda=True --dataset=mnist &
+# This will allow you to use CTRL+C to stop all background processes
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT;
+wait
+python3 scripts/plot_metrics.py --prefix=lda_clustergan
