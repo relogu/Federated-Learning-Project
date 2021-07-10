c0fa592412dbb5341073254064efed94ba61ac44
relogu <lollonasi97@gmail.com>
2021-07-10 11:07:52 +0200

    bug fix

--- script/clustergan_euromds.sh
+++ script/clustergan_euromds.sh
@@ -5,29 +5,29 @@ echo $PWD
 # entire dataset
 python3 py/clustergan.py -g=1 -s=euromds -n=100 -a
 wait
-python3 clustering/scripts/plot_metrics.py -f="$PWD" --prefix=EUROMDS_clustergan_rrrr5k
+python3 clustering/scripts/plot_metrics.py  -f="$PWD" --in_folder="$PWD/output" --prefix=EUROMDS_clustergan_rrrr5k
 sleep 10
 
 # entire dataset
 python3 py/clustergan.py -g=2 -s=euromds -n=100 -a
 wait
-python3 clustering/scripts/plot_metrics.py -f="$PWD" --prefix=EUROMDS_clustergan_rrr5k
+python3 clustering/scripts/plot_metrics.py  -f="$PWD" --in_folder="$PWD/output" --prefix=EUROMDS_clustergan_rrr5k
 sleep 10
 
 # entire dataset
 python3 py/clustergan.py -g=3 -s=euromds -n=100 -a
 wait
-python3 clustering/scripts/plot_metrics.py -f="$PWD" --prefix=EUROMDS_clustergan_rr5k
+python3 clustering/scripts/plot_metrics.py  -f="$PWD" --in_folder="$PWD/output" --prefix=EUROMDS_clustergan_rr5k
 sleep 10
 
 # entire dataset
 python3 py/clustergan.py -g=4 -s=euromds -n=100 -a
 wait
-python3 clustering/scripts/plot_metrics.py -f="$PWD" --prefix=EUROMDS_clustergan_r5k
+python3 clustering/scripts/plot_metrics.py  -f="$PWD" --in_folder="$PWD/output" --prefix=EUROMDS_clustergan_r5k
 sleep 10
 
 # entire dataset
 python3 py/clustergan.py -s=euromds -n=100 -a
 wait
-python3 clustering/scripts/plot_metrics.py -f="$PWD" --prefix=EUROMDS_clustergan5k
+python3 clustering/scripts/plot_metrics.py  -f="$PWD" --in_folder="$PWD/output" --prefix=EUROMDS_clustergan5k
 sleep 10
