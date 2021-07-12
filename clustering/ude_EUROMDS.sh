ddbf24b2ad879e462d1fec48b7b07015ed03d964
relogu <lollonasi97@gmail.com>
2021-07-12 12:54:03 +0200

    utility script

---
+++ clustering/ude_EUROMDS.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+
+# unsupervosed deep embedding using EUROMDS
+python3 py/server.py --strategy=fed_avg --kmeans_epochs=50 --ae_epochs=1000 --cluster_epochs=2000 --n_clients=8 & 
+sleep 2 # Sleep for 2s to give the server enough time to start
+python3 py/client.py --client_id=0 --alg=k-ae_clust --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=1 --alg=k-ae_clust --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=2 --alg=k-ae_clust --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=3 --alg=k-ae_clust --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=4 --alg=k-ae_clust --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=5 --alg=k-ae_clust --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=6 --alg=k-ae_clust --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=7 --alg=k-ae_clust --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS &
+
+# This will allow you to use CTRL+C to stop all background processes
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT;
+wait
+python3 scripts/plot_metrics.py --prefix=EUROMDS-rrrr_ude1k5d2k
+sleep 10
+
+# unsupervosed deep embedding using EUROMDS
+python3 py/server.py --strategy=fed_avg --kmeans_epochs=50 --ae_epochs=1000 --cluster_epochs=2000 --n_clients=8 & 
+sleep 2 # Sleep for 2s to give the server enough time to start
+python3 py/client.py --client_id=0 --alg=k-ae_clust --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=1 --alg=k-ae_clust --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=2 --alg=k-ae_clust --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=3 --alg=k-ae_clust --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=4 --alg=k-ae_clust --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=5 --alg=k-ae_clust --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=6 --alg=k-ae_clust --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=7 --alg=k-ae_clust --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS &
+
+# This will allow you to use CTRL+C to stop all background processes
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT;
+wait
+python3 scripts/plot_metrics.py --prefix=EUROMDS-rrr_ude1k5d2k
+sleep 10
+
+# unsupervosed deep embedding using EUROMDS
+python3 py/server.py --strategy=fed_avg --kmeans_epochs=50 --ae_epochs=1000 --cluster_epochs=2000 --n_clients=8 & 
+sleep 2 # Sleep for 2s to give the server enough time to start
+python3 py/client.py --client_id=0 --alg=k-ae_clust --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=1 --alg=k-ae_clust --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=2 --alg=k-ae_clust --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=3 --alg=k-ae_clust --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=4 --alg=k-ae_clust --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=5 --alg=k-ae_clust --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=6 --alg=k-ae_clust --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=7 --alg=k-ae_clust --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS &
+
+# This will allow you to use CTRL+C to stop all background processes
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT;
+wait
+python3 scripts/plot_metrics.py --prefix=EUROMDS-rr_ude1k5d2k
+sleep 10
+
+# unsupervosed deep embedding using EUROMDS
+python3 py/server.py --strategy=fed_avg --kmeans_epochs=50 --ae_epochs=1000 --cluster_epochs=2000 --n_clients=8 & 
+sleep 2 # Sleep for 2s to give the server enough time to start
+python3 py/client.py --client_id=0 --alg=k-ae_clust --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=1 --alg=k-ae_clust --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=2 --alg=k-ae_clust --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=3 --alg=k-ae_clust --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=4 --alg=k-ae_clust --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=5 --alg=k-ae_clust --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=6 --alg=k-ae_clust --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=7 --alg=k-ae_clust --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS &
+
+# This will allow you to use CTRL+C to stop all background processes
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT;
+wait
+python3 scripts/plot_metrics.py --prefix=EUROMDS-r_ude1k5d2k
+sleep 10
+
+
+# unsupervosed deep embedding using EUROMDS
+python3 py/server.py --strategy=fed_avg --kmeans_epochs=50 --ae_epochs=1000 --cluster_epochs=2000 --n_clients=8 & 
+sleep 2 # Sleep for 2s to give the server enough time to start
+python3 py/client.py --client_id=0 --alg=k-ae_clust --n_clients=8 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=1 --alg=k-ae_clust --n_clients=8 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=2 --alg=k-ae_clust --n_clients=8 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=3 --alg=k-ae_clust --n_clients=8 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=4 --alg=k-ae_clust --n_clients=8 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=5 --alg=k-ae_clust --n_clients=8 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=6 --alg=k-ae_clust --n_clients=8 --n_clusters=10 --dataset=EUROMDS &
+python3 py/client.py --client_id=7 --alg=k-ae_clust --n_clients=8 --n_clusters=10 --dataset=EUROMDS &
+
+# This will allow you to use CTRL+C to stop all background processes
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT;
+wait
+python3 scripts/plot_metrics.py --prefix=EUROMDS_ude1k5d2k
+sleep 10
