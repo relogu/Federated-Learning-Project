13001f5c59ca3a34584436be4a1f62ef83c15670
relogu <lollonasi97@gmail.com>
2021-07-17 11:44:56 +0200

    trivial

--- clustering/clustergan10k_EUROMDS.sh
+++ clustering/clustergan10k_EUROMDS.sh
@@ -3,14 +3,14 @@
 # clustergan with ae clustering using EUROMDS reduced twice
 python3 py/server.py --address=[::]:51552 --strategy=clustergan --total_epochs=10000 --n_clients=8 --out_fol="$PWD/output2" & 
 sleep 2 # Sleep for 2s to give the server enough time to start
-python3 py/client.py --address=[::]:51552 --client_id=0 --alg=clustergan --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=1 --alg=clustergan --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=2 --alg=clustergan --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=3 --alg=clustergan --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=4 --alg=clustergan --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=5 --alg=clustergan --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=6 --alg=clustergan --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=7 --alg=clustergan --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=0 --alg=clustergan --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=1 --alg=clustergan --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=2 --alg=clustergan --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=3 --alg=clustergan --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=4 --alg=clustergan --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=5 --alg=clustergan --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=6 --alg=clustergan --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=7 --alg=clustergan --n_clients=8 --groups=1 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
 
 # This will allow you to use CTRL+C to stop all background processes
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT;
@@ -21,14 +21,14 @@ sleep 10
 # clustergan with ae clustering using EUROMDS reduced twice
 python3 py/server.py --address=[::]:51552 --strategy=clustergan --total_epochs=10000 --n_clients=8 --out_fol="$PWD/output2" & 
 sleep 2 # Sleep for 2s to give the server enough time to start
-python3 py/client.py --address=[::]:51552 --client_id=0 --alg=clustergan --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=1 --alg=clustergan --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=2 --alg=clustergan --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=3 --alg=clustergan --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=4 --alg=clustergan --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=5 --alg=clustergan --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=6 --alg=clustergan --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=7 --alg=clustergan --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=0 --alg=clustergan --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=1 --alg=clustergan --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=2 --alg=clustergan --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=3 --alg=clustergan --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=4 --alg=clustergan --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=5 --alg=clustergan --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=6 --alg=clustergan --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=7 --alg=clustergan --n_clients=8 --groups=2 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
 
 # This will allow you to use CTRL+C to stop all background processes
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT;
@@ -39,14 +39,14 @@ sleep 10
 # clustergan with ae clustering using EUROMDS reduced twice
 python3 py/server.py --address=[::]:51552 --strategy=clustergan --total_epochs=10000 --n_clients=8 --out_fol="$PWD/output2" & 
 sleep 2 # Sleep for 2s to give the server enough time to start
-python3 py/client.py --address=[::]:51552 --client_id=0 --alg=clustergan --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=1 --alg=clustergan --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=2 --alg=clustergan --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=3 --alg=clustergan --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=4 --alg=clustergan --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=5 --alg=clustergan --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=6 --alg=clustergan --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=7 --alg=clustergan --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=0 --alg=clustergan --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=1 --alg=clustergan --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=2 --alg=clustergan --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=3 --alg=clustergan --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=4 --alg=clustergan --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=5 --alg=clustergan --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=6 --alg=clustergan --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=7 --alg=clustergan --n_clients=8 --groups=3 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
 
 # This will allow you to use CTRL+C to stop all background processes
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT;
@@ -57,14 +57,14 @@ sleep 10
 # clustergan with ae clustering using EUROMDS reduced once
 python3 py/server.py --address=[::]:51552 --strategy=clustergan --total_epochs=10000 --n_clients=8 --out_fol="$PWD/output2" & 
 sleep 2 # Sleep for 2s to give the server enough time to start
-python3 py/client.py --address=[::]:51552 --client_id=0 --alg=clustergan --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=1 --alg=clustergan --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=2 --alg=clustergan --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=3 --alg=clustergan --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=4 --alg=clustergan --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=5 --alg=clustergan --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=6 --alg=clustergan --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=7 --alg=clustergan --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=0 --alg=clustergan --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=1 --alg=clustergan --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=2 --alg=clustergan --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=3 --alg=clustergan --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=4 --alg=clustergan --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=5 --alg=clustergan --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=6 --alg=clustergan --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=7 --alg=clustergan --n_clients=8 --groups=4 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
 
 # This will allow you to use CTRL+C to stop all background processes
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT;
@@ -76,14 +76,14 @@ sleep 10
 # clustergan with ae clustering using EUROMDS
 python3 py/server.py --address=[::]:51552 --strategy=clustergan --total_epochs=10000 --n_clients=8 --out_fol="$PWD/output2" & 
 sleep 2 # Sleep for 2s to give the server enough time to start
-python3 py/client.py --address=[::]:51552 --client_id=0 --alg=clustergan --n_clients=8 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=1 --alg=clustergan --n_clients=8 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=2 --alg=clustergan --n_clients=8 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=3 --alg=clustergan --n_clients=8 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=4 --alg=clustergan --n_clients=8 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=5 --alg=clustergan --n_clients=8 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=6 --alg=clustergan --n_clients=8 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
-python3 py/client.py --address=[::]:51552 --client_id=7 --alg=clustergan --n_clients=8 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=0 --alg=clustergan --n_clients=8 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=1 --alg=clustergan --n_clients=8 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=2 --alg=clustergan --n_clients=8 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=3 --alg=clustergan --n_clients=8 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=4 --alg=clustergan --n_clients=8 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=5 --alg=clustergan --n_clients=8 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=6 --alg=clustergan --n_clients=8 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
+python3 py/client.py --server=[::]:51552 --client_id=7 --alg=clustergan --n_clients=8 --n_clusters=10 --dataset=EUROMDS  --out_fol="$PWD/output2" &
 
 # This will allow you to use CTRL+C to stop all background processes
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT;
