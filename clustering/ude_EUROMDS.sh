e1f2dd7a9c7b59100e6bb9f4384a70b63015d0d5
relogu <lollonasi97@gmail.com>
2021-07-23 18:29:12 +0200

    bug fix

--- clustering/ude_EUROMDS.sh
+++ clustering/ude_EUROMDS.sh
@@ -4,14 +4,14 @@
 # unsupervosed deep embedding using EUROMDS
 python3 py/server.py --address=[::]:51551 --strategy=fed_avg --kmeans_epochs=1 --ae_epochs=2 --cluster_epochs=2 --n_clients=8 --out_fol="$PWD/output1" & 
 sleep 2 # Sleep for 2s to give the server enough time to start
-python3 py/client_euromds.py --server=[::]:51551 --client_id=0 --alg=k-ae_clust --n_clients=8 --groups=1 --shuffle=True --fold_n=0 --n_clusters=6 --dataset=EUROMDS --out_fol="$PWD/output1" &
-python3 py/client_euromds.py --server=[::]:51551 --client_id=1 --alg=k-ae_clust --n_clients=8 --groups=1 --shuffle=True --fold_n=0 --n_clusters=6 --dataset=EUROMDS --out_fol="$PWD/output1" &
-python3 py/client_euromds.py --server=[::]:51551 --client_id=2 --alg=k-ae_clust --n_clients=8 --groups=1 --shuffle=True --fold_n=0 --n_clusters=6 --dataset=EUROMDS --out_fol="$PWD/output1" &
-python3 py/client_euromds.py --server=[::]:51551 --client_id=3 --alg=k-ae_clust --n_clients=8 --groups=1 --shuffle=True --fold_n=0 --n_clusters=6 --dataset=EUROMDS --out_fol="$PWD/output1" &
-python3 py/client_euromds.py --server=[::]:51551 --client_id=4 --alg=k-ae_clust --n_clients=8 --groups=1 --shuffle=True --fold_n=0 --n_clusters=6 --dataset=EUROMDS --out_fol="$PWD/output1" &
-python3 py/client_euromds.py --server=[::]:51551 --client_id=5 --alg=k-ae_clust --n_clients=8 --groups=1 --shuffle=True --fold_n=0 --n_clusters=6 --dataset=EUROMDS --out_fol="$PWD/output1" &
-python3 py/client_euromds.py --server=[::]:51551 --client_id=6 --alg=k-ae_clust --n_clients=8 --groups=1 --shuffle=True --fold_n=0 --n_clusters=6 --dataset=EUROMDS --out_fol="$PWD/output1" &
-python3 py/client_euromds.py --server=[::]:51551 --client_id=7 --alg=k-ae_clust --n_clients=8 --groups=1 --shuffle=True --fold_n=0 --n_clusters=6 --dataset=EUROMDS --out_fol="$PWD/output1" &
+python3 py/client_euromds.py --server=[::]:51551 --client_id=0 --alg=k-ae_clust --n_clients=8 --groups=1 --shuffle=True --fold_n=0 --n_clusters=6 --out_fol="$PWD/output1" &
+python3 py/client_euromds.py --server=[::]:51551 --client_id=1 --alg=k-ae_clust --n_clients=8 --groups=1 --shuffle=True --fold_n=0 --n_clusters=6 --out_fol="$PWD/output1" &
+python3 py/client_euromds.py --server=[::]:51551 --client_id=2 --alg=k-ae_clust --n_clients=8 --groups=1 --shuffle=True --fold_n=0 --n_clusters=6 --out_fol="$PWD/output1" &
+python3 py/client_euromds.py --server=[::]:51551 --client_id=3 --alg=k-ae_clust --n_clients=8 --groups=1 --shuffle=True --fold_n=0 --n_clusters=6 --out_fol="$PWD/output1" &
+python3 py/client_euromds.py --server=[::]:51551 --client_id=4 --alg=k-ae_clust --n_clients=8 --groups=1 --shuffle=True --fold_n=0 --n_clusters=6 --out_fol="$PWD/output1" &
+python3 py/client_euromds.py --server=[::]:51551 --client_id=5 --alg=k-ae_clust --n_clients=8 --groups=1 --shuffle=True --fold_n=0 --n_clusters=6 --out_fol="$PWD/output1" &
+python3 py/client_euromds.py --server=[::]:51551 --client_id=6 --alg=k-ae_clust --n_clients=8 --groups=1 --shuffle=True --fold_n=0 --n_clusters=6 --out_fol="$PWD/output1" &
+python3 py/client_euromds.py --server=[::]:51551 --client_id=7 --alg=k-ae_clust --n_clients=8 --groups=1 --shuffle=True --fold_n=0 --n_clusters=6 --out_fol="$PWD/output1" &
 
 # This will allow you to use CTRL+C to stop all background processes
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT;
