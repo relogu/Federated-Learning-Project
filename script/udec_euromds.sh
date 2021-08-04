35b792dba9bad00f8334c7c98b7061e26be06c59
relogu <lollonasi97@gmail.com>
2021-08-04 19:08:54 +0200

    corrected epochs

--- script/udec_euromds.sh
+++ script/udec_euromds.sh
@@ -4,7 +4,7 @@ echo $PWD
 export PYTHONPATH="$PWD:$PYTHONPATH"
 
 # entire dataset
-python3 py/udec/main.py --groups=1 --n_clusters=6 --ae_epochs=20 --cl_epochs=10 --folder="$PWD/output"
+python3 py/udec/main.py --groups=1 --n_clusters=6 --ae_epochs=20000 --cl_epochs=10000 --folder="$PWD/output"
 wait
 python3 py/scripts/plot_metrics.py -f="$PWD" --out_folder="$PWD" --in_folder="$PWD/output" --prefix=EUROMDSrrrr_single_ude20k1u10k
 sleep 10
