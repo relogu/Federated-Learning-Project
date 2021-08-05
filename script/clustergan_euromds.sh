92d1aaaa3283d95fd66d4fa9ddeef35cf9d533c9
relogu <lollonasi97@gmail.com>
2021-08-05 10:40:27 +0200

    corrected arguments

--- script/clustergan_euromds.sh
+++ script/clustergan_euromds.sh
@@ -4,31 +4,31 @@ echo $PWD
 export PYTHONPATH="$PWD:$PYTHONPATH"
 
 # entire dataset
-python3 py/clustergan/main.py -g=1 -s=euromds -n=10000 -a -d=18 --n_clusters=6 --folder="$PWD/output"
+python3 py/clustergan/main.py -g=1 -s=euromds -e=10000 -a -d=18 --n_clusters=6 --folder="$PWD/output"
 wait
 python3 clustering/scripts/plot_metrics.py -f="$PWD" --in_folder="$PWD/output" --prefix=EUROMDSrrrr_single_clustergan10k
 sleep 10
 
 # entire dataset
-python3 py/clustergan/main.py -g=2 -s=euromds -n=10000 -a -d=18 --n_clusters=6 --folder="$PWD/output"
+python3 py/clustergan/main.py -g=2 -s=euromds -e=10000 -a -d=18 --n_clusters=6 --folder="$PWD/output"
 wait
 python3 clustering/scripts/plot_metrics.py -f="$PWD" --in_folder="$PWD/output" --prefix=EUROMDSrrr_single_clustergan10k
 sleep 10
 
 # entire dataset
-python3 py/clustergan/main.py -g=3 -s=euromds -n=10000 -a -d=18 --n_clusters=6 --folder="$PWD/output"
+python3 py/clustergan/main.py -g=3 -s=euromds -e=10000 -a -d=18 --n_clusters=6 --folder="$PWD/output"
 wait
 python3 clustering/scripts/plot_metrics.py -f="$PWD" --in_folder="$PWD/output" --prefix=EUROMDSrr_single_clustergan10k
 sleep 10
 
 # entire dataset
-python3 py/clustergan/main.py -g=4 -s=euromds -n=10000 -a -d=18 --n_clusters=6 --folder="$PWD/output"
+python3 py/clustergan/main.py -g=4 -s=euromds -e=10000 -a -d=18 --n_clusters=6 --folder="$PWD/output"
 wait
 python3 clustering/scripts/plot_metrics.py -f="$PWD" --in_folder="$PWD/output" --prefix=EUROMDSr_single_clustergan10k
 sleep 10
 
 # entire dataset
-python3 py/clustergan/main.py -s=euromds -n=10000 -a -d=18 --n_clusters=6 --folder="$PWD/output"
+python3 py/clustergan/main.py -s=euromds -e=10000 -a -d=18 --n_clusters=6 --folder="$PWD/output"
 wait
 python3 clustering/scripts/plot_metrics.py -f="$PWD" --in_folder="$PWD/output" --prefix=EUROMDS_single_clustergan10k
 sleep 10
