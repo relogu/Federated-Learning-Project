dc2d021f42f6d97c20cc880fc60d76cd3eeccb0a
relogu <lollonasi97@gmail.com>
2021-08-24 13:41:20 +0200

    bug fix on arguments

--- script/fed_udec_euromds.sh
+++ script/fed_udec_euromds.sh
@@ -22,7 +22,7 @@ python3 py/scripts/plot_metrics.py -f="$PWD" --prefix=EUROMDSrrrr_ude20k1u40k --
 sleep 10
 
 # unsupervosed deep embedding using EUROMDS
-python3 clustering/py/server.py --address=[::]:51551 --strategy=fed_avg --kmeans_epochs=1 --ae_epochs=20000 --cluster_epochs=40000--n_clients=8 --out_fol="$PWD/output1" & 
+python3 clustering/py/server.py --address=[::]:51551 --strategy=fed_avg --kmeans_epochs=1 --ae_epochs=20000 --cluster_epochs=40000 --n_clients=8 --out_fol="$PWD/output1" & 
 sleep 2 # Sleep for 2s to give the server enough time to start
 python3 clustering/py/client_euromds.py --server=[::]:51551 --client_id=0 --alg=udec --shuffle=True --fold_n=0 --n_clients=8 --groups=2 --n_clusters=6 --out_fol="$PWD/output1" &
 python3 clustering/py/client_euromds.py --server=[::]:51551 --client_id=1 --alg=udec --shuffle=True --fold_n=0 --n_clients=8 --groups=2 --n_clusters=6 --out_fol="$PWD/output1" &
@@ -40,7 +40,7 @@ python3 py/scripts/plot_metrics.py -f="$PWD" --prefix=EUROMDSrrr_ude20k1u40k --i
 sleep 10
 
 # unsupervosed deep embedding using EUROMDS
-python3 clustering/py/server.py --address=[::]:51551 --strategy=fed_avg --kmeans_epochs=1 --ae_epochs=20000 --cluster_epochs=40000--n_clients=8 --out_fol="$PWD/output1" & 
+python3 clustering/py/server.py --address=[::]:51551 --strategy=fed_avg --kmeans_epochs=1 --ae_epochs=20000 --cluster_epochs=40000 --n_clients=8 --out_fol="$PWD/output1" & 
 sleep 2 # Sleep for 2s to give the server enough time to start
 python3 clustering/py/client_euromds.py --server=[::]:51551 --client_id=0 --alg=udec --shuffle=True --fold_n=0 --n_clients=8 --groups=3 --n_clusters=6 --out_fol="$PWD/output1" &
 python3 clustering/py/client_euromds.py --server=[::]:51551 --client_id=1 --alg=udec --shuffle=True --fold_n=0 --n_clients=8 --groups=3 --n_clusters=6 --out_fol="$PWD/output1" &
@@ -58,7 +58,7 @@ python3 py/scripts/plot_metrics.py -f="$PWD" --prefix=EUROMDSrr_ude20k1u40k --in
 sleep 10
 
 # unsupervosed deep embedding using EUROMDS
-python3 clustering/py/server.py --address=[::]:51551 --strategy=fed_avg --kmeans_epochs=1 --ae_epochs=20000 --cluster_epochs=40000--n_clients=8 --out_fol="$PWD/output1" & 
+python3 clustering/py/server.py --address=[::]:51551 --strategy=fed_avg --kmeans_epochs=1 --ae_epochs=20000 --cluster_epochs=40000 --n_clients=8 --out_fol="$PWD/output1" & 
 sleep 2 # Sleep for 2s to give the server enough time to start
 python3 clustering/py/client_euromds.py --server=[::]:51551 --client_id=0 --alg=udec --shuffle=True --fold_n=0 --n_clients=8 --groups=4 --n_clusters=6 --out_fol="$PWD/output1" &
 python3 clustering/py/client_euromds.py --server=[::]:51551 --client_id=1 --alg=udec --shuffle=True --fold_n=0 --n_clients=8 --groups=4 --n_clusters=6 --out_fol="$PWD/output1" &
@@ -77,7 +77,7 @@ sleep 10
 
 
 # unsupervosed deep embedding using EUROMDS
-python3 clustering/py/server.py --address=[::]:51551 --strategy=fed_avg --kmeans_epochs=1 --ae_epochs=20000 --cluster_epochs=40000--n_clients=8 --out_fol="$PWD/output1" & 
+python3 clustering/py/server.py --address=[::]:51551 --strategy=fed_avg --kmeans_epochs=1 --ae_epochs=20000 --cluster_epochs=40000 --n_clients=8 --out_fol="$PWD/output1" & 
 sleep 2 # Sleep for 2s to give the server enough time to start
 python3 clustering/py/client_euromds.py --server=[::]:51551 --client_id=0 --alg=udec --shuffle=True --fold_n=0 --n_clients=8 --n_clusters=6 --out_fol="$PWD/output1" &
 python3 clustering/py/client_euromds.py --server=[::]:51551 --client_id=1 --alg=udec --shuffle=True --fold_n=0 --n_clients=8 --n_clusters=6 --out_fol="$PWD/output1" &
