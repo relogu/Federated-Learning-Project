b2b4d409f9b8d16d33b9a5fe4d9311e8a78999d2
relogu <lollonasi97@gmail.com>
2021-09-06 12:09:28 +0200

    correction on arguments

--- script/udec_euromds.sh
+++ script/udec_euromds.sh
@@ -4,8 +4,10 @@ echo $PWD
 export PYTHONPATH="$PWD:$PYTHONPATH"
 
 # entire dataset
-python3 py/udec/main.py --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --n_clusters=6 --ae_epochs=20000 --cl_epochs=10000 --folder="$PWD/output" --binary
+python3 py/udec/main.py --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --n_clusters=6 --ae_epochs=20000 --cl_epochs=10000 --folder="$PWD/output1" --binary
 wait
+mkdir "$PWD/results/EUROMDSrrrr_single_binary_ude20k1u40k"
+mv "$PWD/output1/"/* "$PWD/results/EUROMDSrrrr_single_binary_ude20k1u40k"/
 #python3 py/scripts/plot_metrics.py -f="$PWD" --out_folder="$PWD" --in_folder="$PWD/output" --prefix=EUROMDSrrrr_single_prob_ude20k1u10k
 sleep 10
 
