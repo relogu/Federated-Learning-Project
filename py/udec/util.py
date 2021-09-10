63f810ec77f995771cbd4d59145819e8ca49e668
relogu <lollonasi97@gmail.com>
2021-09-10 17:39:15 +0200

    added some aes and mod to hyperparams

--- py/udec/util.py
+++ py/udec/util.py
@@ -6,16 +6,119 @@ Created on Wen Aug 4 10:37:10 2021
 @author: relogu
 """
 import tensorflow as tf
-from tensorflow.keras.layers import InputLayer, Dense, Dropout
+from tensorflow.keras.layers import InputLayer, Dense, Dropout, GaussianNoise
 from tensorflow.keras.models import Sequential, Model
 import tensorflow_probability as tfp
 from tensorflow_probability import distributions as tfd
 from tensorflow_probability import layers as tfpl
 
-from py.udec.net import DenseTied, ClusteringLayer
+from py.udec.net import DenseTied, ClusteringLayer, FlippingNoise
+
+# some string for verbose outputs
+encoder_layer_name = 'encoder_%d'
+decoder_layer_name = 'decoder_%d'
+list_enc_dim = 'List of encoder dimensions: {}'
+list_dec_dim = 'List of decoder dimensions: {}'
+enc_verb = 'Encoder Layer {}: {} with dims {}'
+dec_verb = 'Decoder Layer {}: {} with dims {}'
+dec_tied_verb = 'Decoder Layer {}: {} with dims {}, tied to {}'
+enc_layers = 'Encoder Layers: {}'
+dec_layers = 'Decoder Layers: {}'
+ae_layers = 'Autoencoder Layers: {}'
 
 
-def create_autoencoder(dims, act='relu', init='glorot_uniform', verbose=False):
+def create_denoising_autoencoder(dims,
+                                 up_freq,
+                                 act='relu',
+                                 init='glorot_uniform',
+                                 dropout=False,
+                                 verbose=False):
+    """
+    Fully connected auto-encoder model, symmetric, using DenseTied layers.
+    Arguments:
+        dims: list of number of units in each layer of encoder. dims[0] is input dim, dims[-1] is units in hidden layer.
+            The decoder is symmetric with encoder. So number of layers of the auto-encoder is 2*len(dims)-1
+        up_freq: list of frequencies of the up values in the training set
+        act: activation, not applied to Input, Hidden and Output layers
+        init: initializator of the layers' weights
+        verbose: print some useful information on the layers' complexity
+    return:
+        (autoencoder, encoder, decoder), Model of autoencoder, Model of encoder, Model of decoder
+    """
+    # getting encoder and decoder layers output dim
+    encoder_dims = dims[1:]
+    decoder_dims = list(reversed(dims))[1:]
+    if verbose:
+        print(list_enc_dim.format(encoder_dims))
+        print(list_dec_dim.format(decoder_dims))
+    # input data
+    input_img = InputLayer(input_shape=(dims[0],), name='input_img')
+    # input labels
+    input_lbl = InputLayer(input_shape=(dims[-1],), name='input_lbl')
+    # encoder
+    encoder_layers = []
+    encoder_layers.append(input_img)
+    # internal layers in encoder
+    for i in range(len(encoder_dims)):
+        x = Dense(units=encoder_dims[i],
+                  activation=act,
+                  kernel_initializer=init,
+                  use_bias=True,
+                  name=encoder_layer_name % i)
+        if verbose:
+            print(enc_verb.format(
+                encoder_layer_name % i, x, encoder_dims[i]))
+        encoder_layers.append(x)
+
+    # decoder
+    decoder_layers = []
+    decoder_layers.append(input_lbl)
+    # internal layers in decoder
+    for i in range(len(decoder_dims)):
+        if i == len(decoder_dims)-1:
+            act = 'sigmoid'
+        x = Dense(units=decoder_dims[i],
+                  activation=act,
+                  kernel_initializer=init,
+                  use_bias=True,
+                  name=decoder_layer_name % i)
+        if verbose:
+            print(dec_verb.format(
+                decoder_layer_name % i, x, decoder_dims[i], encoder_layers[len(encoder_layers)-1-i]))
+        decoder_layers.append(x)
+
+    # adding dropout
+    if dropout:
+        encoder_layers.insert(2, Dropout(0.2))
+    #encoder_layers.insert(1, GaussianNoise(0.2))
+    encoder_layers.insert(1, FlippingNoise(up_frequencies=up_freq, rate=0.1))
+    
+    # autoencoder
+    autoencoder_layers = []
+    autoencoder_layers = autoencoder_layers + encoder_layers
+    autoencoder_layers = autoencoder_layers + decoder_layers[1:]
+
+    # defining models
+    autoencoder = Sequential(autoencoder_layers, name='AE')
+    encoder = Sequential(encoder_layers, name='encoder')
+    decoder = Sequential(decoder_layers, name='decoder')
+
+    if verbose:
+        print(enc_layers.format(encoder_layers))
+        print(dec_layers.format(decoder_layers))
+        print(ae_layers.format(autoencoder_layers))
+        autoencoder.summary()
+        encoder.summary()
+        decoder.summary()
+
+    return (autoencoder, encoder, decoder)
+
+
+def create_tied_autoencoder(dims,
+                            act='relu',
+                            init='glorot_uniform',
+                            dropout=False,
+                            verbose=False):
     """
     Fully connected auto-encoder model, symmetric, using DenseTied layers.
     Arguments:
@@ -31,8 +134,8 @@ def create_autoencoder(dims, act='relu', init='glorot_uniform', verbose=False):
     encoder_dims = dims[1:]
     decoder_dims = list(reversed(dims))[1:]
     if verbose:
-        print('List of encoder dimensions: {}'.format(encoder_dims))
-        print('List of decoder dimensions: {}'.format(decoder_dims))
+        print(list_enc_dim.format(encoder_dims))
+        print(list_dec_dim.format(decoder_dims))
     # input data
     input_img = InputLayer(input_shape=(dims[0],), name='input_img')
     # input labels
@@ -48,10 +151,10 @@ def create_autoencoder(dims, act='relu', init='glorot_uniform', verbose=False):
                   kernel_initializer=init,
                   # kernel_constraint=UnitNorm(axis=0),
                   use_bias=False,  # True,
-                  name='encoder_%d' % i)
+                  name=encoder_layer_name % i)
         if verbose:
-            print('Encoder Layer {}: {} with dims {}'.format(
-                'encoder_%d' % i, x, encoder_dims[i]))
+            print(enc_verb.format(
+                encoder_layer_name % i, x, encoder_dims[i]))
         encoder_layers.append(x)
 
     # decoder
@@ -68,11 +171,15 @@ def create_autoencoder(dims, act='relu', init='glorot_uniform', verbose=False):
                       # kernel_initializer=init,
                       use_bias=False,  # True,
                       # kernel_constraint=UnitNorm(axis=1),
-                      name='decoder_%d' % i)
+                      name=decoder_layer_name % i)
         if verbose:
-            print('Decoder Layer {}: {} with dims {}, tied to {}'.format(
-                'decoder_%d' % i, x, decoder_dims[i], encoder_layers[len(encoder_layers)-1-i]))
+            print(dec_tied_verb.format(
+                decoder_layer_name % i, x, decoder_dims[i], encoder_layers[len(encoder_layers)-1-i]))
         decoder_layers.append(x)
+    
+    # adding dropout
+    if dropout:
+        encoder_layers.insert(1, Dropout(rate=0.2))
 
     # autoencoder
     autoencoder_layers = []
@@ -85,9 +192,103 @@ def create_autoencoder(dims, act='relu', init='glorot_uniform', verbose=False):
     decoder = Sequential(decoder_layers, name='decoder')
 
     if verbose:
-        print('Encoder Layers: {}'.format(encoder_layers))
-        print('Decoder Layers: {}'.format(decoder_layers))
-        print('Autoencoder Layers: {}'.format(autoencoder_layers))
+        print(enc_layers.format(encoder_layers))
+        print(dec_layers.format(decoder_layers))
+        print(ae_layers.format(autoencoder_layers))
+        autoencoder.summary()
+        encoder.summary()
+        decoder.summary()
+
+    return (autoencoder, encoder, decoder)
+
+
+def create_tied_prob_autoencoder(dims,
+                                 prob_layer=tfpl.IndependentBernoulli,
+                                 distr=tfd.Bernoulli.logits,
+                                 act=tf.nn.leaky_relu,
+                                 init='glorot_uniform',
+                                 dropout=False,
+                                 verbose=False):
+    """
+    Fully connected auto-encoder model, symmetric, using DenseTied layers with a probabilistic layer on top of the decoder.
+    Arguments:
+        dims: list of number of units in each layer of encoder. dims[0] is input dim, dims[-1] is units in hidden layer.
+            The decoder is symmetric with encoder. So number of layers of the auto-encoder is 2*len(dims)-1
+        prob_layer: probabilistic layer from tensorflow probability to put on top of the decoder
+        distr: probability distribution onto which the probailistic layer is built
+        act: activation, not applied to Input, Hidden and Output layers
+        init: initializator of the layers' weights
+        verbose: print some useful information on the layers' complexity
+    return:
+        (autoencoder, encoder, decoder), Model of autoencoder, Model of encoder, Model of decoder
+    """
+    # getting encoder and decoder layers output dim
+    encoder_dims = dims[1:]
+    decoder_dims = list(reversed(dims))[1:]
+    if verbose:
+        print(liste_enc_dim.format(encoder_dims))
+        print(liste_dec_dim.format(decoder_dims))
+    # input data
+    input_img = InputLayer(input_shape=(dims[0],), name='input_img')
+    # input labels
+    input_lbl = InputLayer(input_shape=(dims[-1],), name='input_lbl')
+    # encoder
+    encoder_layers = []
+    encoder_layers.append(input_img)
+    # internal layers in encoder
+    for i in range(len(encoder_dims)):
+        x = Dense(units=encoder_dims[i],
+                  activation=act,
+                  #kernel_regularizer=WeightsOrthogonalityConstraint(encoder_dims[i], weightage=1., axis=0),
+                  kernel_initializer=init,
+                  # kernel_constraint=UnitNorm(axis=0),
+                  use_bias=False,  # True,
+                  name=encoder_layer_name % i)
+        if verbose:
+            print(enc_verb.format(
+                encoder_layer_name % i, x, encoder_dims[i]))
+        encoder_layers.append(x)
+
+    # decoder
+    decoder_layers = []
+    decoder_layers.append(input_lbl)
+    # internal layers in decoder
+    for i in range(len(decoder_dims)):
+        if i == len(decoder_dims)-1:
+            act = 'sigmoid'
+        x = DenseTied(tied_to=encoder_layers[len(encoder_layers)-1-i],
+                      units=decoder_dims[i],
+                      activation=act,
+                      #kernel_regularizer=WeightsOrthogonalityConstraint(encoder_dims[len(encoder_dims)-1-i], weightage=1., axis=1),
+                      # kernel_initializer=init,
+                      use_bias=False,  # True,
+                      # kernel_constraint=UnitNorm(axis=1),
+                      name=decoder_layer_name % i)
+        if verbose:
+            print(dec_tied_verb.format(
+                decoder_layer_name % i, x, decoder_dims[i], encoder_layers[len(encoder_layers)-1-i]))
+        decoder_layers.append(x)
+    decoder_layers.append(
+        prob_layer(dims[0], distr))
+    
+    # adding dropout
+    if dropout:
+        encoder_layers.insert(1, Dropout(rate=0.2))
+
+    # autoencoder
+    autoencoder_layers = []
+    autoencoder_layers = autoencoder_layers + encoder_layers
+    autoencoder_layers = autoencoder_layers + decoder_layers[1:]
+
+    # defining models
+    autoencoder = Sequential(autoencoder_layers, name='AE')
+    encoder = Sequential(encoder_layers, name='encoder')
+    decoder = Sequential(decoder_layers, name='decoder')
+
+    if verbose:
+        print(enc_layers.format(encoder_layers))
+        print(dec_layers.format(decoder_layers))
+        print(ae_layers.format(autoencoder_layers))
         autoencoder.summary()
         encoder.summary()
         decoder.summary()
@@ -100,6 +301,7 @@ def create_prob_autoencoder(dims,
                             distr=tfd.Bernoulli.logits,
                             act=tf.nn.leaky_relu,
                             init='glorot_uniform',
+                            dropout=False,
                             verbose=False):
     """
     Fully connected auto-encoder model, symmetric, using DenseTied layers with a probabilistic layer on top of the decoder.
@@ -118,8 +320,8 @@ def create_prob_autoencoder(dims,
     encoder_dims = dims[1:]
     decoder_dims = list(reversed(dims))[1:]
     if verbose:
-        print('List of encoder dimensions: {}'.format(encoder_dims))
-        print('List of decoder dimensions: {}'.format(decoder_dims))
+        print(list_enc_dim.format(encoder_dims))
+        print(list_dec_dim.format(decoder_dims))
     # input data
     input_img = InputLayer(input_shape=(dims[0],), name='input_img')
     # input labels
@@ -131,14 +333,12 @@ def create_prob_autoencoder(dims,
     for i in range(len(encoder_dims)):
         x = Dense(units=encoder_dims[i],
                   activation=act,
-                  #kernel_regularizer=WeightsOrthogonalityConstraint(encoder_dims[i], weightage=1., axis=0),
                   kernel_initializer=init,
-                  # kernel_constraint=UnitNorm(axis=0),
-                  use_bias=False,  # True,
-                  name='encoder_%d' % i)
+                  use_bias=True,
+                  name=encoder_layer_name % i)
         if verbose:
-            print('Encoder Layer {}: {} with dims {}'.format(
-                'encoder_%d' % i, x, encoder_dims[i]))
+            print(enc_verb.format(
+                encoder_layer_name % i, x, encoder_dims[i]))
         encoder_layers.append(x)
 
     # decoder
@@ -148,25 +348,24 @@ def create_prob_autoencoder(dims,
     for i in range(len(decoder_dims)):
         if i == len(decoder_dims)-1:
             act = 'sigmoid'
-        x = DenseTied(tied_to=encoder_layers[len(encoder_layers)-1-i],
-                      units=decoder_dims[i],
-                      activation=act,
-                      #kernel_regularizer=WeightsOrthogonalityConstraint(encoder_dims[len(encoder_dims)-1-i], weightage=1., axis=1),
-                      # kernel_initializer=init,
-                      use_bias=False,  # True,
-                      # kernel_constraint=UnitNorm(axis=1),
-                      name='decoder_%d' % i)
+        x = Dense(units=decoder_dims[i],
+                  activation=act,
+                  kernel_initializer=init,
+                  use_bias=True,
+                  name=decoder_layer_name % i)
         if verbose:
-            print('Decoder Layer {}: {} with dims {}, tied to {}'.format(
-                'decoder_%d' % i, x, decoder_dims[i], encoder_layers[len(encoder_layers)-1-i]))
+            print(dec_tied_verb.format(
+                decoder_layer_name % i,
+                x,
+                decoder_dims[i],
+                encoder_layers[len(encoder_layers)-1-i]))
         decoder_layers.append(x)
     decoder_layers.append(
         prob_layer(dims[0], distr))
     
     # adding dropout
-    for i in range(len(encoder_dims)):
-        #encoder_layers.insert(int(2*i + 1), Dropout(rate=0.2)) # not a good accuracy
-        encoder_layers.insert(int(2*i + 1), Dropout(rate=0.1))
+    if dropout:
+        encoder_layers.insert(1, Dropout(rate=0.2))
 
     # autoencoder
     autoencoder_layers = []
@@ -179,9 +378,9 @@ def create_prob_autoencoder(dims,
     decoder = Sequential(decoder_layers, name='decoder')
 
     if verbose:
-        print('Encoder Layers: {}'.format(encoder_layers))
-        print('Decoder Layers: {}'.format(decoder_layers))
-        print('Autoencoder Layers: {}'.format(autoencoder_layers))
+        print(enc_layers.format(encoder_layers))
+        print(dec_layers.format(decoder_layers))
+        print(ae_layers.format(autoencoder_layers))
         autoencoder.summary()
         encoder.summary()
         decoder.summary()
