9fb51a966a8fac956ba32663b0dd60b7fe8673c0
relogu <lollonasi97@gmail.com>
2021-09-13 17:50:13 +0200

    trivial

--- script/udec_euromds.sh
+++ script/udec_euromds.sh
@@ -5,104 +5,30 @@ export PYTHONPATH="$PWD:$PYTHONPATH"
 
 mkdir "$PWD/output_udec"
 # entire dataset
-python3 py/udec/main.py --dropout 0.2 --ran_flip 0.2 --cl_lr 0.01 --update_interval 1000 --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --n_clusters=6 --ae_epochs=10000 --cl_epochs=1 --folder="$PWD/output_udec" --hardware_acc
+python3 py/udec/main.py --tied --dropout 0.01 --ran_flip 0.02 --cl_lr 0.01 --update_interval 1000 --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --n_clusters=6 --ae_epochs=10000 --cl_epochs=1 --folder="$PWD/output_udec" --hardware_acc
 wait
 sleep 5
-mkdir "$PWD/results/EUROMDSfinal_deno_single_ude10k1u1u_do02_rf01"
-mv "$PWD/output_udec"/* "$PWD/results/EUROMDSfinal_deno_single_ude10k1u1u_do02_rf01"/
+mkdir "$PWD/results/tiedEUROMDSfinal_deno_single_ude10k1u1u_do001_rf002"
+mv "$PWD/output_udec"/* "$PWD/results/tiedEUROMDSfinal_deno_single_ude10k1u1u_do001_rf002"/
 sleep 10
 rmdir "$PWD/output_udec"
 
 mkdir "$PWD/output_udec"
 # entire dataset
-python3 py/udec/main.py --dropout 0.1 --ran_flip 0.2 --cl_lr 0.01 --update_interval 1000 --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --n_clusters=6 --ae_epochs=10000 --cl_epochs=1 --folder="$PWD/output_udec" --hardware_acc
+python3 py/udec/main.py --tied --dropout 0.01 --ran_flip 0.03 --cl_lr 0.01 --update_interval 1000 --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --n_clusters=6 --ae_epochs=10000 --cl_epochs=1 --folder="$PWD/output_udec" --hardware_acc
 wait
 sleep 5
-mkdir "$PWD/results/EUROMDSfinal_deno_single_ude10k1u1u_do01_rf01"
-mv "$PWD/output_udec"/* "$PWD/results/EUROMDSfinal_deno_single_ude10k1u1u_do01_rf01"/
+mkdir "$PWD/results/tiedEUROMDSfinal_deno_single_ude10k1u1u_do001_rf003"
+mv "$PWD/output_udec"/* "$PWD/results/tiedEUROMDSfinal_deno_single_ude10k1u1u_do001_rf003"/
 sleep 10
 rmdir "$PWD/output_udec"
 
 mkdir "$PWD/output_udec"
 # entire dataset
-python3 py/udec/main.py --dropout 0.05 --ran_flip 0.2 --cl_lr 0.01 --update_interval 1000 --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --n_clusters=6 --ae_epochs=10000 --cl_epochs=1 --folder="$PWD/output_udec" --hardware_acc
+python3 py/udec/main.py --tied --dropout 0.01 --ran_flip 0.04 --cl_lr 0.01 --update_interval 1000 --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --n_clusters=6 --ae_epochs=10000 --cl_epochs=1 --folder="$PWD/output_udec" --hardware_acc
 wait
 sleep 5
-mkdir "$PWD/results/EUROMDSfinal_deno_single_ude10k1u1u_do005_rf01"
-mv "$PWD/output_udec"/* "$PWD/results/EUROMDSfinal_deno_single_ude10k1u1u_do005_rf01"/
+mkdir "$PWD/results/tiedEUROMDSfinal_deno_single_ude10k1u1u_do001_rf004"
+mv "$PWD/output_udec"/* "$PWD/results/tiedEUROMDSfinal_deno_single_ude10k1u1u_do001_rf004"/
 sleep 10
 rmdir "$PWD/output_udec"
-
-mkdir "$PWD/output_udec"
-# entire dataset
-python3 py/udec/main.py --dropout 0.01 --ran_flip 0.2 --cl_lr 0.01 --update_interval 1000 --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --n_clusters=6 --ae_epochs=10000 --cl_epochs=1 --folder="$PWD/output_udec" --hardware_acc
-wait
-sleep 5
-mkdir "$PWD/results/EUROMDSfinal_deno_single_ude10k1u1u_do001_rf01"
-mv "$PWD/output_udec"/* "$PWD/results/EUROMDSfinal_deno_single_ude10k1u1u_do001_rf01"/
-sleep 10
-rmdir "$PWD/output_udec"
-
-# mkdir "$PWD/output_udec"
-# # entire dataset
-# python3 py/udec/main.py --cl_lr 0.01 --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --n_clusters=6 --ae_epochs=10000 --cl_epochs=40000 --folder="$PWD/output_udec" --hardware_acc
-# wait
-# sleep 5
-# mkdir "$PWD/results/EUROMDSfinal_deno_single_ude10k1u40k"
-# mv "$PWD/output_udec"/* "$PWD/results/EUROMDSfinal_deno_single_ude10k1u40k"/
-# sleep 10
-# rmdir "$PWD/output_udec"
-
-# mkdir "$PWD/output_udec"
-# # entire dataset
-# python3 py/udec/main.py --cl_lr 0.01 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --n_clusters=6 --ae_epochs=10000 --cl_epochs=40000 --folder="$PWD/output_udec" --hardware_acc
-# wait
-# sleep 5
-# mkdir "$PWD/results/tiedEUROMDSfinal_deno_single_ude10k1u40k"
-# mv "$PWD/output_udec"/* "$PWD/results/tiedEUROMDSfinal_deno_single_ude10k1u40k"/
-# sleep 10
-# rmdir "$PWD/output_udec"
-
-# mkdir "$PWD/output_udec"
-# # entire dataset
-# python3 py/udec/main.py --cl_lr 0.001 --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --n_clusters=6 --ae_epochs=10000 --cl_epochs=60000 --folder="$PWD/output_udec" --hardware_acc
-# wait
-# sleep 5
-# mkdir "$PWD/results/EUROMDSfinal_deno_single_ude10k1u60k"
-# mv "$PWD/output_udec"/* "$PWD/results/EUROMDSfinal_deno_single_ude10k1u60k"/
-# sleep 10
-# rmdir "$PWD/output_udec"
-
-# mkdir "$PWD/output_udec"
-# # entire dataset
-# python3 py/udec/main.py --cl_lr 0.001 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --n_clusters=6 --ae_epochs=10000 --cl_epochs=60000 --folder="$PWD/output_udec" --hardware_acc
-# wait
-# sleep 5
-# mkdir "$PWD/results/tiedEUROMDSfinal_deno_single_ude10k1u60k"
-# mv "$PWD/output_udec"/* "$PWD/results/tiedEUROMDSfinal_deno_single_ude10k1u60k"/
-# sleep 10
-# rmdir "$PWD/output_udec"
-
-# # entire dataset
-# python3 py/udec/main.py --groups=2 --n_clusters=6 --ae_epochs=20000 --cl_epochs=10000 --folder="$PWD/output"
-# wait
-# python3 py/scripts/plot_metrics.py  -f="$PWD" --out_folder="$PWD" --in_folder="$PWD/output" --prefix=EUROMDSrrr_single_ude20k1u10k
-# sleep 10
-
-# # entire dataset
-# python3 py/udec/main.py --groups=3 --n_clusters=6 --ae_epochs=20000 --cl_epochs=10000 --folder="$PWD/output"
-# wait
-# python3 py/scripts/plot_metrics.py  -f="$PWD" --out_folder="$PWD" --in_folder="$PWD/output" --prefix=EUROMDSrr_single_ude20k1u10k
-# sleep 10
-
-# # entire dataset
-# python3 py/udec/main.py --groups=4 --n_clusters=6 --ae_epochs=20000 --cl_epochs=10000 --folder="$PWD/output"
-# wait
-# python3 py/scripts/plot_metrics.py  -f="$PWD" --out_folder="$PWD" --in_folder="$PWD/output" --prefix=EUROMDSr_single_ude20k1u10k
-# sleep 10
-
-# # entire dataset
-# python3 py/udec/main.py --n_clusters=6 --ae_epochs=20000 --cl_epochs=10000 --folder="$PWD/output"
-# wait
-# python3 py/scripts/plot_metrics.py  -f="$PWD" --out_folder="$PWD" --in_folder="$PWD/output" --prefix=EUROMDS_single_ude20k1u10k
-# sleep 10
