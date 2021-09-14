2e56c9c7795bdd85beb9887bd56ceff8287f7e7b
relogu <lollonasi97@gmail.com>
2021-09-14 13:57:00 +0200

    corrections to folders

--- script/udec_euromds2.sh
+++ script/udec_euromds2.sh
@@ -7,22 +7,34 @@ mkdir "$PWD/output_udec2"
 cp "$PWD/results/tied_uiEUROMDSfinal_deno_single_ude10k1u1u_do001_rf005/encoder.npz" "$PWD/output_udec2/encoder.npz"
 cp "$PWD/results/tied_uiEUROMDSfinal_deno_single_ude10k1u1u_do001_rf005/ae_history" "$PWD/output_udec2/ae_history"
 # entire dataset
-python3 py/udec/main.py --tied --dropout 0.01 --ran_flip 0.05 --cl_lr 0.1 --update_interval 100 --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --n_clusters=6 --ae_epochs=10000 --cl_epochs=20000 --folder="$PWD/output_udec2" --hardware_acc
+python3 py/udec/main.py --tied --dropout 0.01 --ran_flip 0.05 --cl_lr 0.1 --update_interval 1 --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --n_clusters=6 --ae_epochs=10000 --cl_epochs=20000 --folder="$PWD/output_udec2" --hardware_acc
 wait
 sleep 5
-mkdir "$PWD/results/tied_ui100_clEUROMDSfinal_deno_single_ude10k1u20k_do001_rf005"
-mv "$PWD/output_udec2"/* "$PWD/results/tied_ui100_clEUROMDSfinal_deno_single_ude10k1u20k_do001_rf005"/
+mkdir "$PWD/results/tied_ui1u_clEUROMDSfinal_deno_single_ude10k1u20k_do001_rf005"
+mv "$PWD/output_udec2"/* "$PWD/results/tied_ui1u_clEUROMDSfinal_deno_single_ude10k1u20k_do001_rf005"/
 sleep 10
 rmdir "$PWD/output_udec2"
 
-mkdir "$PWD/output_udec2"
-cp "$PWD/results/tied_uiEUROMDSfinal_deno_single_ude10k1u1u_do001_rf005/encoder.npz" "$PWD/output_udec2/encoder.npz"
-cp "$PWD/results/tied_uiEUROMDSfinal_deno_single_ude10k1u1u_do001_rf005/ae_history" "$PWD/output_udec2/ae_history"
-# entire dataset
-python3 py/udec/main.py --tied --dropout 0.01 --ran_flip 0.05 --cl_lr 0.1 --update_interval 20002 --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --n_clusters=6 --ae_epochs=10000 --cl_epochs=20000 --folder="$PWD/output_udec2" --hardware_acc
-wait
-sleep 5
-mkdir "$PWD/results/tied_ui500_clEUROMDSfinal_deno_single_ude10k1u20k_do001_rf005"
-mv "$PWD/output_udec2"/* "$PWD/results/tied_ui500_clEUROMDSfinal_deno_single_ude10k1u20k_do001_rf005"/
-sleep 10
-rmdir "$PWD/output_udec2"
+# mkdir "$PWD/output_udec2"
+# cp "$PWD/results/tied_uiEUROMDSfinal_deno_single_ude10k1u1u_do001_rf005/encoder.npz" "$PWD/output_udec2/encoder.npz"
+# cp "$PWD/results/tied_uiEUROMDSfinal_deno_single_ude10k1u1u_do001_rf005/ae_history" "$PWD/output_udec2/ae_history"
+# # entire dataset
+# python3 py/udec/main.py --tied --dropout 0.01 --ran_flip 0.05 --cl_lr 0.1 --update_interval 100 --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --n_clusters=6 --ae_epochs=10000 --cl_epochs=20000 --folder="$PWD/output_udec2" --hardware_acc
+# wait
+# sleep 5
+# mkdir "$PWD/results/tied_ui100_clEUROMDSfinal_deno_single_ude10k1u20k_do001_rf005"
+# mv "$PWD/output_udec2"/* "$PWD/results/tied_ui100_clEUROMDSfinal_deno_single_ude10k1u20k_do001_rf005"/
+# sleep 10
+# rmdir "$PWD/output_udec2"
+
+# mkdir "$PWD/output_udec2"
+# cp "$PWD/results/tied_uiEUROMDSfinal_deno_single_ude10k1u1u_do001_rf005/encoder.npz" "$PWD/output_udec2/encoder.npz"
+# cp "$PWD/results/tied_uiEUROMDSfinal_deno_single_ude10k1u1u_do001_rf005/ae_history" "$PWD/output_udec2/ae_history"
+# # entire dataset
+# python3 py/udec/main.py --tied --dropout 0.01 --ran_flip 0.05 --cl_lr 0.1 --update_interval 500 --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --n_clusters=6 --ae_epochs=10000 --cl_epochs=20000 --folder="$PWD/output_udec2" --hardware_acc
+# wait
+# sleep 5
+# mkdir "$PWD/results/tied_ui500_clEUROMDSfinal_deno_single_ude10k1u20k_do001_rf005"
+# mv "$PWD/output_udec2"/* "$PWD/results/tied_ui500_clEUROMDSfinal_deno_single_ude10k1u20k_do001_rf005"/
+# sleep 10
+# rmdir "$PWD/output_udec2"
