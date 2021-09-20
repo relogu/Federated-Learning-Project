25cebb89e01c3dabf42bfcf92d0c82433671f899
relogu <lollonasi97@gmail.com>
2021-09-20 11:52:26 +0200

    tests

--- script/fed_udec_euromds.sh
+++ script/fed_udec_euromds.sh
@@ -7,14 +7,14 @@ mkdir "$PWD/output_fed_udec"
 # UDEC using EUROMDS
 python3 clustering/py/server.py --address=[::]:51550 --n_clusters 8 --strategy=k-fed --kmeans_epochs=1 --ae_epochs=1 --cluster_epochs=10000 --n_clients=2 --out_fol="$PWD/output_fed_udec" & 
 sleep 2 # Sleep for 2s to give the server enough time to start
-python3 clustering/py/client_euromds.py --n_clusters 8 --ran_flip 0.25 --server=[::]:51550 --update_interval 200000 --tied --client_id=0 --alg=udec --shuffle=True --fold_n=0 --n_clients=8 --groups=Genetics --groups=CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --out_fol="$PWD/output_fed_udec" &
-python3 clustering/py/client_euromds.py --n_clusters 8 --ran_flip 0.25 --server=[::]:51550 --update_interval 200000 --tied --client_id=1 --alg=udec --shuffle=True --fold_n=0 --n_clients=8 --groups=Genetics --groups=CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --out_fol="$PWD/output_fed_udec" &
+python3 clustering/py/client_euromds.py --n_clusters 8 --ran_flip 0.25 --server=[::]:51550 --update_interval 100 --n_clients=2 --tied --client_id=0 --alg=udec --groups=Genetics --groups=CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --out_fol="$PWD/output_fed_udec" &
+python3 clustering/py/client_euromds.py --n_clusters 8 --ran_flip 0.25 --server=[::]:51550 --update_interval 100 --n_clients=2 --tied --client_id=1 --alg=udec --groups=Genetics --groups=CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --out_fol="$PWD/output_fed_udec" &
 
 # This will allow you to use CTRL+C to stop all background processes
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT;
 wait
-mkdir "$PWD/results/kFED_noui2CEUROMDSfinal_deno_ude10k1u10k"
-mv "$PWD/output_fed_udec/"/* "$PWD/results/kFED_noui2CEUROMDSfinal_deno_ude10k1u10k"/
+mkdir "$PWD/results/8kFED_ui2CEUROMDSfinal_deno_ude2k1u10k"
+mv "$PWD/output_fed_udec/"/* "$PWD/results/8kFED_ui2CEUROMDSfinal_deno_ude2k1u10k"/
 #python3 py/scripts/plot_metrics.py -f="$PWD" --prefix=EUROMDSfinal_deno_ude20k1u40k --in_folder="$PWD/results/EUROMDSfinal_deno_ude20k1u40k"
 sleep 10
 rmdir "$PWD/output_fed_udec"
