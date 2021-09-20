625b411b56ab168a6507cb436b1a5d8289fb91fe
relogu <lollonasi97@gmail.com>
2021-09-20 14:38:15 +0200

    tests

--- script/fed_udec_euromds3.sh
+++ script/fed_udec_euromds3.sh
@@ -26,7 +26,7 @@ export PYTHONPATH="$PWD:$PYTHONPATH"
 
 mkdir "$PWD/output_fed_udec3"
 # UDEC using EUROMDS
-python3 clustering/py/server.py --n_clusters 8 --address=[::]:51553 --strategy=k-fed --kmeans_epochs=1 --ae_epochs=2000 --cluster_epochs=10000 --n_clients=8 --out_fol="$PWD/output_fed_udec3" & 
+python3 clustering/py/server.py --n_clusters 8 --address=[::]:51553 --strategy=k-fed --kmeans_epochs=1 --ae_epochs=10000 --cluster_epochs=10000 --n_clients=8 --out_fol="$PWD/output_fed_udec3" & 
 sleep 2 # Sleep for 2s to give the server enough time to start
 python3 clustering/py/client_euromds.py --n_clusters 8 --ran_flip 0.25 --server=[::]:51553 --update_interval 100 --n_clients=8 --tied --client_id=0 --groups=Genetics --groups=CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --out_fol="$PWD/output_fed_udec3" &
 python3 clustering/py/client_euromds.py --n_clusters 8 --ran_flip 0.25 --server=[::]:51553 --update_interval 100 --n_clients=8 --tied --client_id=1 --groups=Genetics --groups=CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D --out_fol="$PWD/output_fed_udec3" &
@@ -40,8 +40,8 @@ python3 clustering/py/client_euromds.py --n_clusters 8 --ran_flip 0.25 --server=
 # This will allow you to use CTRL+C to stop all background processes
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT;
 wait
-mkdir "$PWD/results/8kFED_ui8CEUROMDSfinal_deno_ude2k1u10k"
-mv "$PWD/output_fed_udec3/"/* "$PWD/results/8kFED_ui8CEUROMDSfinal_deno_ude2k1u10k"/
+mkdir "$PWD/results/8kFED_ui8CEUROMDSfinal_deno_ude10k1u10k"
+mv "$PWD/output_fed_udec3/"/* "$PWD/results/8kFED_ui8CEUROMDSfinal_deno_ude10k1u10k"/
 #python3 py/scripts/plot_metrics.py -f="$PWD" --prefix=EUROMDSfinal_deno_ude20k1u40k --in_folder="$PWD/results/EUROMDSfinal_deno_ude20k1u40k"
 sleep 10
 rmdir "$PWD/output_fed_udec3"
