1b0d5a13e793f3022a33d85d1d5b233dbdd3535d
relogu <lollonasi97@gmail.com>
2021-09-24 10:56:54 +0200

    added out fol

--- script/udec_euromds.sh
+++ script/udec_euromds.sh
@@ -4,73 +4,75 @@ echo $PWD
 export PYTHONPATH="$PWD:$PYTHONPATH"
 echo "Number of clusters $1"
 CLUSTERS="$1"
+echo "Output folder $2"
+OUT_FOL="$2"
 
-mkdir "$PWD/output_udec"
+mkdir "$PWD/$OUT_FOL"
 # entire dataset
-python3 py/udec/main.py --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.20 --ran_flip 0.20 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/output_udec" --hardware_acc
+python3 py/udec/main.py --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.20 --ran_flip 0.20 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/$OUT_FOL" --hardware_acc
 wait
 sleep 5
 mkdir "$PWD/results/DEC_EUROMDSfinal_single_a_K$CLUSTERS"
-mv "$PWD/output_udec"/* "$PWD/results/DEC_EUROMDSfinal_single_a_K$CLUSTERS"/
+mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDSfinal_single_a_K$CLUSTERS"/
 sleep 10
-rmdir "$PWD/output_udec"
+rmdir "$PWD/$OUT_FOL"
 
-mkdir "$PWD/output_udec"
+mkdir "$PWD/$OUT_FOL"
 # entire dataset
-python3 py/udec/main.py --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.05 --ran_flip 0.05 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/output_udec" --hardware_acc
+python3 py/udec/main.py --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.05 --ran_flip 0.05 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/$OUT_FOL" --hardware_acc
 wait
 sleep 5
 mkdir "$PWD/results/DEC_EUROMDSfinal_single_b_K$CLUSTERS"
-mv "$PWD/output_udec"/* "$PWD/results/DEC_EUROMDSfinal_single_b_K$CLUSTERS"/
+mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDSfinal_single_b_K$CLUSTERS"/
 sleep 10
-rmdir "$PWD/output_udec"
+rmdir "$PWD/$OUT_FOL"
 
-mkdir "$PWD/output_udec"
+mkdir "$PWD/$OUT_FOL"
 # entire dataset
-python3 py/udec/main.py --u_norm --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.20 --ran_flip 0.20 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/output_udec" --hardware_acc
+python3 py/udec/main.py --u_norm --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.20 --ran_flip 0.20 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/$OUT_FOL" --hardware_acc
 wait
 sleep 5
 mkdir "$PWD/results/DEC_EUROMDSfinal_single_c_K$CLUSTERS"
-mv "$PWD/output_udec"/* "$PWD/results/DEC_EUROMDSfinal_single_c_K$CLUSTERS"/
+mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDSfinal_single_c_K$CLUSTERS"/
 sleep 10
-rmdir "$PWD/output_udec"
+rmdir "$PWD/$OUT_FOL"
 
-mkdir "$PWD/output_udec"
+mkdir "$PWD/$OUT_FOL"
 # entire dataset
-python3 py/udec/main.py --u_norm --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.10 --ran_flip 0.10 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/output_udec" --hardware_acc
+python3 py/udec/main.py --u_norm --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.10 --ran_flip 0.10 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/$OUT_FOL" --hardware_acc
 wait
 sleep 5
 mkdir "$PWD/results/DEC_EUROMDSfinal_single_d_K$CLUSTERS"
-mv "$PWD/output_udec"/* "$PWD/results/DEC_EUROMDSfinal_single_d_K$CLUSTERS"/
+mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDSfinal_single_d_K$CLUSTERS"/
 sleep 10
-rmdir "$PWD/output_udec"
+rmdir "$PWD/$OUT_FOL"
 
-mkdir "$PWD/output_udec"
+mkdir "$PWD/$OUT_FOL"
 # entire dataset
-python3 py/udec/main.py --u_norm --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.05 --ran_flip 0.05 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/output_udec" --hardware_acc
+python3 py/udec/main.py --u_norm --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.05 --ran_flip 0.05 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/$OUT_FOL" --hardware_acc
 wait
 sleep 5
 mkdir "$PWD/results/DEC_EUROMDSfinal_single_e_K$CLUSTERS"
-mv "$PWD/output_udec"/* "$PWD/results/DEC_EUROMDSfinal_single_e_K$CLUSTERS"/
+mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDSfinal_single_e_K$CLUSTERS"/
 sleep 10
-rmdir "$PWD/output_udec"
+rmdir "$PWD/$OUT_FOL"
 
-mkdir "$PWD/output_udec"
+mkdir "$PWD/$OUT_FOL"
 # entire dataset
-python3 py/udec/main.py --u_norm --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.01 --ran_flip 0.01 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/output_udec" --hardware_acc
+python3 py/udec/main.py --u_norm --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.01 --ran_flip 0.01 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/$OUT_FOL" --hardware_acc
 wait
 sleep 5
 mkdir "$PWD/results/DEC_EUROMDSfinal_single_f_K$CLUSTERS"
-mv "$PWD/output_udec"/* "$PWD/results/DEC_EUROMDSfinal_single_f_K$CLUSTERS"/
+mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDSfinal_single_f_K$CLUSTERS"/
 sleep 10
-rmdir "$PWD/output_udec"
+rmdir "$PWD/$OUT_FOL"
 
-mkdir "$PWD/output_udec"
+mkdir "$PWD/$OUT_FOL"
 # entire dataset
-python3 py/udec/main.py --u_norm --ae_epochs 5000 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.01 --ran_flip 0.01 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/output_udec" --hardware_acc
+python3 py/udec/main.py --u_norm --ae_epochs 5000 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.01 --ran_flip 0.01 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/$OUT_FOL" --hardware_acc
 wait
 sleep 5
 mkdir "$PWD/results/DEC_EUROMDSfinal_single_g_K$CLUSTERS"
-mv "$PWD/output_udec"/* "$PWD/results/DEC_EUROMDSfinal_single_g_K$CLUSTERS"/
+mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDSfinal_single_g_K$CLUSTERS"/
 sleep 10
-rmdir "$PWD/output_udec"
+rmdir "$PWD/$OUT_FOL"
