d678fa221310a325ab5b8b45fc45de97c1070df5
relogu <lollonasi97@gmail.com>
2021-10-04 09:36:02 +0200

    added ortho flag choice

--- script/udec_euromds.sh
+++ script/udec_euromds.sh
@@ -15,73 +15,81 @@ FILL="--fill"
 DS="fill"
 fi
 echo "Dataset type chosen $DS"
+echo "Use orthogonality constraint $4"
+ORTHO=""
+if [ $4 = "y" ]
+then
+ORTHO="--ortho"
+fi
+
+DATASET="--groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D"
 
 mkdir "$PWD/$OUT_FOL"
 # entire dataset
-python3 py/udec/main.py $FILL --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.20 --ran_flip 0.20 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/$OUT_FOL" --hardware_acc
+python3 py/udec/main.py $FILL $ORTHO --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.20 --ran_flip 0.20 --tied $DATASET --folder "$PWD/$OUT_FOL" --hardware_acc
 wait
 sleep 5
-mkdir "$PWD/results/DEC_EUROMDS{$DS}_single_a_K$CLUSTERS"
-mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDS{$DS}_single_a_K$CLUSTERS"/
+mkdir "$PWD/results/DEC_EUROMDS{$DS}_single_a{$ORTHO}_K$CLUSTERS"
+mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDS{$DS}_single_a{$ORTHO}_K$CLUSTERS"/
 sleep 10
 rmdir "$PWD/$OUT_FOL"
 
 mkdir "$PWD/$OUT_FOL"
 # entire dataset
-python3 py/udec/main.py $FILL --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.05 --ran_flip 0.05 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/$OUT_FOL" --hardware_acc
+python3 py/udec/main.py $FILL $ORTHO --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.05 --ran_flip 0.05 --tied $DATASET --folder "$PWD/$OUT_FOL" --hardware_acc
 wait
 sleep 5
-mkdir "$PWD/results/DEC_EUROMDS{$DS}_single_b_K$CLUSTERS"
-mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDS{$DS}_single_b_K$CLUSTERS"/
+mkdir "$PWD/results/DEC_EUROMDS{$DS}_single_b{$ORTHO}_K$CLUSTERS"
+mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDS{$DS}_single_b{$ORTHO}_K$CLUSTERS"/
 sleep 10
 rmdir "$PWD/$OUT_FOL"
 
 mkdir "$PWD/$OUT_FOL"
 # entire dataset
-python3 py/udec/main.py $FILL --u_norm --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.20 --ran_flip 0.20 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/$OUT_FOL" --hardware_acc
+python3 py/udec/main.py $FILL $ORTHO --u_norm --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.20 --ran_flip 0.20 --tied $DATASET --folder "$PWD/$OUT_FOL" --hardware_acc
 wait
 sleep 5
-mkdir "$PWD/results/DEC_EUROMDS{$DS}_single_c_K$CLUSTERS"
-mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDS{$DS}_single_c_K$CLUSTERS"/
+mkdir "$PWD/results/DEC_EUROMDS{$DS}_single_c{$ORTHO}_K$CLUSTERS"
+mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDS{$DS}_single_c{$ORTHO}_K$CLUSTERS"/
 sleep 10
 rmdir "$PWD/$OUT_FOL"
 
 mkdir "$PWD/$OUT_FOL"
 # entire dataset
-python3 py/udec/main.py $FILL --u_norm --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.10 --ran_flip 0.10 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/$OUT_FOL" --hardware_acc
+python3 py/udec/main.py $FILL $ORTHO --u_norm --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.10 --ran_flip 0.10 --tied $DATASET --folder "$PWD/$OUT_FOL" --hardware_acc
 wait
 sleep 5
-mkdir "$PWD/results/DEC_EUROMDS{$DS}_single_d_K$CLUSTERS"
-mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDS{$DS}_single_d_K$CLUSTERS"/
+mkdir "$PWD/results/DEC_EUROMDS{$DS}_single_d{$ORTHO}_K$CLUSTERS"
+mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDS{$DS}_single_d{$ORTHO}_K$CLUSTERS"/
 sleep 10
 rmdir "$PWD/$OUT_FOL"
 
 mkdir "$PWD/$OUT_FOL"
 # entire dataset
-python3 py/udec/main.py $FILL --u_norm --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.05 --ran_flip 0.05 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/$OUT_FOL" --hardware_acc
+python3 py/udec/main.py $FILL $ORTHO --u_norm --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.05 --ran_flip 0.05 --tied $DATASET --folder "$PWD/$OUT_FOL" --hardware_acc
 wait
 sleep 5
-mkdir "$PWD/results/DEC_EUROMDS{$DS}_single_e_K$CLUSTERS"
-mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDS{$DS}_single_e_K$CLUSTERS"/
+mkdir "$PWD/results/DEC_EUROMDS{$DS}_single_e{$ORTHO}_K$CLUSTERS"
+mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDS{$DS}_single_e{$ORTHO}_K$CLUSTERS"/
 sleep 10
 rmdir "$PWD/$OUT_FOL"
 
 mkdir "$PWD/$OUT_FOL"
 # entire dataset
-python3 py/udec/main.py $FILL --u_norm --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.01 --ran_flip 0.01 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/$OUT_FOL" --hardware_acc
+python3 py/udec/main.py $FILL $ORTHO --u_norm --ae_epochs 2500 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.01 --ran_flip 0.01 --tied $DATASET --folder "$PWD/$OUT_FOL" --hardware_acc
 wait
 sleep 5
-mkdir "$PWD/results/DEC_EUROMDS{$DS}_single_f_K$CLUSTERS"
-mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDS{$DS}_single_f_K$CLUSTERS"/
+mkdir "$PWD/results/DEC_EUROMDS{$DS}_single_f{$ORTHO}_K$CLUSTERS"
+mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDS{$DS}_single_f{$ORTHO}_K$CLUSTERS"/
 sleep 10
 rmdir "$PWD/$OUT_FOL"
 
 mkdir "$PWD/$OUT_FOL"
 # entire dataset
-python3 py/udec/main.py $FILL --u_norm --ae_epochs 5000 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.01 --ran_flip 0.01 --tied --groups Genetics --groups CNA --ex_col UTX --ex_col CSF3R --ex_col SETBP1 --ex_col PPM1D  --folder "$PWD/$OUT_FOL" --hardware_acc
+python3 py/udec/main.py $FILL $ORTHO --u_norm --ae_epochs 5000 --cl_epochs 20000 --n_clusters $CLUSTERS --dropout 0.01 --ran_flip 0.01 --tied $DATASET --folder "$PWD/$OUT_FOL" --hardware_acc
 wait
 sleep 5
-mkdir "$PWD/results/DEC_EUROMDS{$DS}_single_g_K$CLUSTERS"
-mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDS{$DS}_single_g_K$CLUSTERS"/
+mkdir "$PWD/results/DEC_EUROMDS{$DS}_single_g{$ORTHO}_K$CLUSTERS"
+mv "$PWD/$OUT_FOL"/* "$PWD/results/DEC_EUROMDS{$DS}_single_g{$ORTHO}_K$CLUSTERS"/
 sleep 10
 rmdir "$PWD/$OUT_FOL"
