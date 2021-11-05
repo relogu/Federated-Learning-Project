134b299d87e9b097c5fe4017e36f43a3f27ff123
relogu <lollonasi97@gmail.com>
2021-11-05 12:47:15 +0100

    little corrections

--- py/losses/keras/cosine_similarity.py
+++ py/losses/keras/cosine_similarity.py
@@ -2,12 +2,12 @@ import tensorflow as tf
 import tensorflow.keras.backend as K
 
 
-def CosineSimilarityLoss(targets, inputs, smooth=1e-6):
+def CosineSimilarityLoss(targets, inputs):
     """
     Cosine Distance is a classic vector distance metric that 
     is used commonly when comparing Bag of Words representations 
     in NLP problems. The distance is calculated by finding the 
-    cosine angle between the two vectors
+    cosine angle between the two vectors.
     """
     
     # flatten label and prediction tensors
@@ -18,4 +18,4 @@ def CosineSimilarityLoss(targets, inputs, smooth=1e-6):
     
     y_true = tf.linalg.l2_normalize(targets, axis=0)
     y_pred = tf.linalg.l2_normalize(inputs, axis=0)
-    return -tf.reduce_sum(y_true * y_pred)
+    return 1 - tf.reduce_sum(y_true * y_pred)
