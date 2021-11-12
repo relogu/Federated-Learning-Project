89cb0a843eb3f1b18e1f0feaf730d74ec1c28f30
relogu <lollonasi97@gmail.com>
2021-11-12 16:34:40 +0100

    tests

--- py/dec_euromds.py
+++ py/dec_euromds.py
@@ -248,7 +248,7 @@ if __name__ == "__main__":
                 500,#int((2)*(n_features)),#int((2/3)*(n_features)),
                 500,#int((2)*(n_features)),#int((2/3)*(n_features)),
                 2000,#int((3)*(n_features)),#int((2.5)*(n_features)),
-                args.n_clusters],  # DEC paper proportions
+                5],#args.n_clusters],  # DEC paper proportions
         # 'relu' --> DEC paper # 'selu' --> is better for binary
         'act': 'selu',
         'ae_metrics': [my_metrics.rounded_accuracy,
