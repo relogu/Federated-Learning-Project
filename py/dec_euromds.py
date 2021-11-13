80fde71cff9b6707be5ce427f99c2c40989e52a3
relogu <lollonasi97@gmail.com>
2021-11-13 15:36:57 +0100

    bug fix on layers width

--- py/dec_euromds.py
+++ py/dec_euromds.py
@@ -245,9 +245,9 @@ if __name__ == "__main__":
         #                     stddev=0.2)  # stddev=0.01), # DEC paper, is better
         'init': GlorotUniform(seed=51550),
         'dims': [n_features,
-                150,500,#int((2)*(n_features)),#int((2/3)*(n_features)),
-                150,500,#int((2)*(n_features)),#int((2/3)*(n_features)),
-                500,2000,#int((3)*(n_features)),#int((2.5)*(n_features)),
+                150,#500,#int((2)*(n_features)),#int((2/3)*(n_features)),
+                150,#500,#int((2)*(n_features)),#int((2/3)*(n_features)),
+                500,#2000,#int((3)*(n_features)),#int((2.5)*(n_features)),
                 5],#args.n_clusters],  # DEC paper proportions
         # 'relu' --> DEC paper # 'selu' --> is better for binary
         'act': 'selu',
