0c6125ef74df882736e3ee3d9d930ab4de290174
relogu <lollonasi97@gmail.com>
2021-11-19 10:35:09 +0100

    correct to use probs

--- py/losses/keras/dice_bce.py
+++ py/losses/keras/dice_bce.py
@@ -8,7 +8,11 @@ def DiceBCELoss(targets, inputs, smooth=1e-6):
     This loss combines Dice loss with the standard binary cross-entropy (BCE)
     loss that is generally the default for segmentation models.
     Combining the two methods allows for some diversity in the loss,
-    while benefitting from the stability of BCE. 
+    while benefitting from the stability of BCE.
+    Dice-Coefficient for probability is computed as:
+    DC = |A*B|/(|A|+|B|-|A*B|), where |A*B| is intersection
+    Dice-Coefficient loss is 1-DC, that could be rewritten as:
+    DCl = (|A|+|B|)/(|A|+|B|-|A*B|)
     """
 
     # flatten label and prediction tensors
@@ -16,11 +20,14 @@ def DiceBCELoss(targets, inputs, smooth=1e-6):
     # r_inputs = K.round(inputs) # can't be done in loss fn
     targets = K.flatten(targets)
     c_targets = tf.cast(targets, tf.float32)
-
     intersection = K.sum(K.dot(tf.expand_dims(c_targets, axis=0),
                                K.transpose(tf.expand_dims(inputs, axis=0))))
-    dice = 1 - (2*intersection + smooth) / \
-        (K.sum(c_targets) + K.sum(inputs) + smooth)
+
+    # OLD
+    # dice = 1 - (2*intersection + smooth) / \
+    #     (K.sum(c_targets) + K.sum(inputs) + smooth)
+    dice = (K.sum(c_targets) + K.sum(inputs) + smooth)/ \
+        (K.sum(c_targets) + K.sum(inputs) - intersection + smooth)
     bce = binary_crossentropy(targets, inputs)
     dice_bce = bce + dice
 
