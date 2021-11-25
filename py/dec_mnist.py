51bdd377187bebe8e57bf472c887db87ce95e17d
relogu <lollonasi97@gmail.com>
2021-11-25 16:16:12 +0100

    tests

--- py/dec_mnist.py
+++ py/dec_mnist.py
@@ -94,55 +94,56 @@ if __name__ == "__main__":
         compute_devices=gpus[1:],
         parameter_device=gpus[0]
     )
-    with strategy.scope():
-        
-        # preparing dataset
-        (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-        n_features = int(x_train.shape[1]*x_train.shape[2])
-        x_train, x_test = x_train.reshape(x_train.shape[0], n_features)/255, x_test.reshape(x_test.shape[0], n_features)/255
-        
-        # initializing common configuration dict
-        initial_learning_rate = 0.1
-        
-        def lr_step_decay(epoch, lr):
-            # lr is divided by 10 every 20000 rounds
-            drop_rate = 10
-            epoch_drop = 20000
-            lr = initial_learning_rate
-            if epoch > epoch_drop:
-                lr = initial_learning_rate/(drop_rate**int(epoch/epoch_drop))
-            return lr
-        
-        config = {
-            'batch_size': 256,
-            'n_clusters': args.n_clusters,
-            'kmeans_n_init': 20,
-            'ae_epochs': 50000,
-            'ae_optimizer': SGD(
-                learning_rate=0.1,
-                momentum=0.9,
-                decay=(0.1-0.0001)/50000),
-            'ae_init': RandomNormal(mean=0.0,
-                                    stddev=0.01),
-            'ae_dims': [784,
-                500,
-                500,
-                2000,
-                10],
-            'ae_metrics': [my_metrics.rounded_accuracy],
-            'cl_optimizer': SGD(
-                learning_rate=0.01,
-                momentum=0.9),
-            'update_interval': args.update_interval,
-            'ae_loss': 'mse',
-            'cl_loss': 'kld',
-            'seed': args.seed}
+    strategy = tf.distribute.MirroredStrategy(gpus)
+    
+    # preparing dataset
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    n_features = int(x_train.shape[1]*x_train.shape[2])
+    x_train, x_test = x_train.reshape(x_train.shape[0], n_features)/255, x_test.reshape(x_test.shape[0], n_features)/255
+    
+    # initializing common configuration dict
+    initial_learning_rate = 0.1
+    
+    def lr_step_decay(epoch, lr):
+        # lr is divided by 10 every 20000 rounds
+        drop_rate = 10
+        epoch_drop = 20000
+        lr = initial_learning_rate
+        if epoch > epoch_drop:
+            lr = initial_learning_rate/(drop_rate**int(epoch/epoch_drop))
+        return lr
+    
+    config = {
+        'batch_size': 256,
+        'n_clusters': args.n_clusters,
+        'kmeans_n_init': 20,
+        'ae_epochs': 50000,
+        'ae_optimizer': SGD(
+            learning_rate=0.1,
+            momentum=0.9,
+            decay=(0.1-0.0001)/50000),
+        'ae_init': RandomNormal(mean=0.0,
+                                stddev=0.01),
+        'ae_dims': [784,
+            500,
+            500,
+            2000,
+            10],
+        'ae_metrics': [my_metrics.rounded_accuracy],
+        'cl_optimizer': SGD(
+            learning_rate=0.01,
+            momentum=0.9),
+        'update_interval': args.update_interval,
+        'ae_loss': 'mse',
+        'cl_loss': 'kld',
+        'seed': args.seed}
 
-        # TODO: Gready Layer-Wise pretrain of the autoencoder necessary
-        pretrained_weights = path_to_out/'encoder.npz'
-        if not pretrained_weights.exists():
-            print('There are no existing weights in the output folder for the autoencoder')
-            
+    # TODO: Gready Layer-Wise pretrain of the autoencoder necessary
+    pretrained_weights = path_to_out/'encoder.npz'
+    if not pretrained_weights.exists():
+        print('There are no existing weights in the output folder for the autoencoder')
+        
+        with strategy.scope():
             autoencoder, encoder, decoder = create_dec_sae(
                 dims=config['ae_dims'],
                 init=config['ae_init'])
@@ -154,32 +155,34 @@ if __name__ == "__main__":
                 optimizer=config['ae_optimizer'],
                 loss=config['ae_loss']
             )
-            
-            # fitting the autoencoder
-            history = autoencoder.fit(x=x_train,
-                                    y=x_train,
-                                    batch_size=config['batch_size'],
-                                    epochs=int(config['ae_epochs']),
-                                    validation_data=(x_test, x_test),
-                                    callbacks=[#LearningRateScheduler(lr_step_decay, verbose=1),
-                                                EarlyStopping(
-                                                    patience=5000,
-                                                    verbose=1,
-                                                    mode="auto",
-                                                    baseline=None,
-                                                    restore_best_weights=False,)
-                                                ],
-                                    verbose=2)
-            with open(path_to_out/'pretrain_ae_history', 'wb') as file_pi:
-                pickle.dump(history.history, file_pi)
-            parameters = np.array(encoder.get_weights(), dtype=object)
-            np.savez(path_to_out/'encoder', parameters)
+        
+        # fitting the autoencoder
+        history = autoencoder.fit(x=x_train,
+                                y=x_train,
+                                batch_size=config['batch_size'],
+                                epochs=int(config['ae_epochs']),
+                                validation_data=(x_test, x_test),
+                                callbacks=[#LearningRateScheduler(lr_step_decay, verbose=1),
+                                            EarlyStopping(
+                                                patience=5000,
+                                                verbose=1,
+                                                mode="auto",
+                                                baseline=None,
+                                                restore_best_weights=False,)
+                                            ],
+                                verbose=2)
+        with open(path_to_out/'pretrain_ae_history', 'wb') as file_pi:
+            pickle.dump(history.history, file_pi)
+        parameters = np.array(encoder.get_weights(), dtype=object)
+        np.savez(path_to_out/'encoder', parameters)
 
-        trained_weights = path_to_out/'encoder_ft.npz'
-        if not trained_weights.exists():
-            param = np.load(pretrained_weights, allow_pickle=True)
-            weights = np.array([param[p] for p in param])[0]
-            print('There are no existing weights in the output folder for the autoencoder')
+    trained_weights = path_to_out/'encoder_ft.npz'
+    if not trained_weights.exists():
+        param = np.load(pretrained_weights, allow_pickle=True)
+        weights = np.array([param[p] for p in param])[0]
+        print('There are no existing weights in the output folder for the autoencoder')
+        
+        with strategy.scope():
             autoencoder, encoder, decoder = create_dec_sae(
                 dims=config['ae_dims'],
                 init=config['ae_init'],
@@ -193,49 +196,53 @@ if __name__ == "__main__":
                 loss=config['ae_loss']
             )
 
-            # fitting again the autoencoder
-            history = autoencoder.fit(x=x_train,
-                                    y=x_train,
-                                    batch_size=config['batch_size'],
-                                    epochs=int(2*config['ae_epochs']),
-                                    validation_data=(x_test, x_test),
-                                    callbacks=[#LearningRateScheduler(lr_step_decay, verbose=1),
-                                                EarlyStopping(
-                                                    patience=5000,
-                                                    verbose=1,
-                                                    mode="auto",
-                                                    baseline=None,
-                                                    restore_best_weights=False,)
-                                                ],
-                                    verbose=2)
-            with open(path_to_out/'finetune_ae_history', 'wb') as file_pi:
-                pickle.dump(history.history, file_pi)
-            parameters = np.array(encoder.get_weights(), dtype=object)
-            np.savez(path_to_out/'encoder_ft', parameters)
+        # fitting again the autoencoder
+        history = autoencoder.fit(x=x_train,
+                                y=x_train,
+                                batch_size=config['batch_size'],
+                                epochs=int(2*config['ae_epochs']),
+                                validation_data=(x_test, x_test),
+                                callbacks=[#LearningRateScheduler(lr_step_decay, verbose=1),
+                                            EarlyStopping(
+                                                patience=5000,
+                                                verbose=1,
+                                                mode="auto",
+                                                baseline=None,
+                                                restore_best_weights=False,)
+                                            ],
+                                verbose=2)
+        with open(path_to_out/'finetune_ae_history', 'wb') as file_pi:
+            pickle.dump(history.history, file_pi)
+        parameters = np.array(encoder.get_weights(), dtype=object)
+        np.savez(path_to_out/'encoder_ft', parameters)
 
+
+    with strategy.scope():
         # clean from the auxialiary layer for the clustering model
         autoencoder, encoder, decoder = create_dec_sae(
             dims=config['ae_dims'],
             init=config['ae_init'],
             dropout_rate=0.0)
 
-        param = np.load(trained_weights, allow_pickle=True)
-        weights = np.array([param[p] for p in param])[0]
-        encoder.set_weights(weights)
+    param = np.load(trained_weights, allow_pickle=True)
+    weights = np.array([param[p] for p in param])[0]
+    encoder.set_weights(weights)
 
-        # get an estimate for clusters centers using k-means
-        z = encoder(x_train).numpy()
-        kmeans = KMeans(
-            init='k-means++',
-            n_clusters=config['n_clusters'],
-            # number of different random initializations
-            n_init=config['kmeans_n_init'],
-        ).fit(z)
-        # saving the model weights
-        initial_centroids = np.array([kmeans.cluster_centers_])
-        print('Saving initial centroids')
-        np.savez(path_to_out/'initial_centroids', initial_centroids)
+    # get an estimate for clusters centers using k-means
+    z = encoder(x_train).numpy()
+    kmeans = KMeans(
+        init='k-means++',
+        n_clusters=config['n_clusters'],
+        # number of different random initializations
+        n_init=config['kmeans_n_init'],
+    ).fit(z)
+    # saving the model weights
+    initial_centroids = np.array([kmeans.cluster_centers_])
+    print('Saving initial centroids')
+    np.savez(path_to_out/'initial_centroids', initial_centroids)
 
+
+    with strategy.scope():
         # training the clustering model
         clustering_model = create_clustering_model(
             config['n_clusters'],
@@ -247,82 +254,82 @@ if __name__ == "__main__":
             loss=config['cl_loss'])
         clustering_model.get_layer(
             name='clustering').set_weights(initial_centroids)
-        y_old = None
-        train_loss, eval_loss = 0.1, 0
-        i = 0
-        while True:
-            i += 1
-            # if i % config['update_interval'] == 1:
-            #     # if train_loss < eval_loss:
-            print('Shuffling data')
-            idx = np.random.permutation(len(x_train))
-            x_train = x_train[idx, :]
-            print('Updating the target distribution')
-            train_q = clustering_model(x_train).numpy()
-            # update the auxiliary target distribution p
-            train_p = target_distribution(train_q)
-            clustering_model.fit(x=x_train,
-                                y=train_p,
-                                verbose=2,
-                                batch_size=config['batch_size'],
-                                steps_per_epoch=config['update_interval'])
-            # evaluation
-            q = clustering_model(x_train).numpy()
-            # update the auxiliary target distribution p
-            p = target_distribution(q)
-            # retrieving loss
-            loss = clustering_model.evaluate(x_train, p, verbose=2)
-            # test
-            q = clustering_model(x_test).numpy()
-            # update the auxiliary target distribution p
-            p = target_distribution(q)
-            # retrieving loss
-            loss = clustering_model.evaluate(x_test, p, verbose=2)
-            # evaluate the clustering performance using some metrics
-            y_pred = q.argmax(1)
-            # getting the cycle accuracy of evaluation set
-            x_ae_test = autoencoder(x_test)
-            y_ae_pred = clustering_model(
-                np.round(x_ae_test)).numpy().argmax(1)
-            cycle_acc = my_metrics.acc(y_pred, y_ae_pred)
-            del y_ae_pred, x_ae_test
-            # evaluating metrics
-            result = {}
-            if y_test is not None and args.verbose:
-                acc = my_metrics.acc(y_test, y_pred)
-                nmi = my_metrics.nmi(y_test, y_pred)
-                print('DEC Clustering\nEpoch %d\n\tacc %.5f\n\tnmi %.5f' %
-                    (i, acc, nmi))
-                print('Cycle accuracy is {}'.format(cycle_acc))
-                # dumping and retrieving the results
-                metrics = {'accuracy': acc,
-                        'normalized_mutual_info_score': nmi,}
-                result = metrics.copy()
-            result['cycle_accuracy'] = cycle_acc
-            result['loss'] = eval_loss
-            result['round'] = i
-            # check for required convergence
-            if i > 1:
-                tol = float(1 - my_metrics.acc(y_pred, y_old))
-                if i % 100 and args.verbose:
-                    print("Current label change ratio is {}, i.e. {}/{} samples".
-                        format(tol, int(tol*len(x_train)), len(x_train)))
-                if tol < 0.001:
-                    print("Final label change ratio is {}, i.e. {}/{} samples, reached after {} iteration".
-                        format(tol, int(tol*len(x_train)), len(x_train), i))
-                    break
-                else:
-                    y_old = y_pred.copy()
+    y_old = None
+    train_loss, eval_loss = 0.1, 0
+    i = 0
+    while True:
+        i += 1
+        # if i % config['update_interval'] == 1:
+        #     # if train_loss < eval_loss:
+        print('Shuffling data')
+        idx = np.random.permutation(len(x_train))
+        x_train = x_train[idx, :]
+        print('Updating the target distribution')
+        train_q = clustering_model(x_train).numpy()
+        # update the auxiliary target distribution p
+        train_p = target_distribution(train_q)
+        clustering_model.fit(x=x_train,
+                            y=train_p,
+                            verbose=2,
+                            batch_size=config['batch_size'],
+                            steps_per_epoch=config['update_interval'])
+        # evaluation
+        q = clustering_model(x_train).numpy()
+        # update the auxiliary target distribution p
+        p = target_distribution(q)
+        # retrieving loss
+        loss = clustering_model.evaluate(x_train, p, verbose=2)
+        # test
+        q = clustering_model(x_test).numpy()
+        # update the auxiliary target distribution p
+        p = target_distribution(q)
+        # retrieving loss
+        loss = clustering_model.evaluate(x_test, p, verbose=2)
+        # evaluate the clustering performance using some metrics
+        y_pred = q.argmax(1)
+        # getting the cycle accuracy of evaluation set
+        x_ae_test = autoencoder(x_test)
+        y_ae_pred = clustering_model(
+            np.round(x_ae_test)).numpy().argmax(1)
+        cycle_acc = my_metrics.acc(y_pred, y_ae_pred)
+        del y_ae_pred, x_ae_test
+        # evaluating metrics
+        result = {}
+        if y_test is not None and args.verbose:
+            acc = my_metrics.acc(y_test, y_pred)
+            nmi = my_metrics.nmi(y_test, y_pred)
+            print('DEC Clustering\nEpoch %d\n\tacc %.5f\n\tnmi %.5f' %
+                (i, acc, nmi))
+            print('Cycle accuracy is {}'.format(cycle_acc))
+            # dumping and retrieving the results
+            metrics = {'accuracy': acc,
+                    'normalized_mutual_info_score': nmi,}
+            result = metrics.copy()
+        result['cycle_accuracy'] = cycle_acc
+        result['loss'] = eval_loss
+        result['round'] = i
+        # check for required convergence
+        if i > 1:
+            tol = float(1 - my_metrics.acc(y_pred, y_old))
+            if i % 100 and args.verbose:
+                print("Current label change ratio is {}, i.e. {}/{} samples".
+                    format(tol, int(tol*len(x_train)), len(x_train)))
+            if tol < 0.001:
+                print("Final label change ratio is {}, i.e. {}/{} samples, reached after {} iteration".
+                    format(tol, int(tol*len(x_train)), len(x_train), i))
+                break
             else:
-                tol = 1
                 y_old = y_pred.copy()
-            result['tol'] = tol
-            dump_result_dict('clustering_model', result,
-                            path_to_out=path_to_out)
+        else:
+            tol = 1
+            y_old = y_pred.copy()
+        result['tol'] = tol
+        dump_result_dict('clustering_model', result,
+                        path_to_out=path_to_out)
 
-        # saving the model weights
-        parameters = np.array(encoder.get_weights(), dtype=object)
-        np.savez(path_to_out/'encoder_final', parameters)
+    # saving the model weights
+    parameters = np.array(encoder.get_weights(), dtype=object)
+    np.savez(path_to_out/'encoder_final', parameters)
 
-        parameters = np.array(clustering_model.get_layer(name='clustering').get_weights(), dtype=object)
-        np.savez(path_to_out/'final_centroids', parameters)
+    parameters = np.array(clustering_model.get_layer(name='clustering').get_weights(), dtype=object)
+    np.savez(path_to_out/'final_centroids', parameters)
