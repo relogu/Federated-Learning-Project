d6fc703f126c2e7a4a01d0e8f516f2f40a47628c
relogu <lollonasi97@gmail.com>
2021-12-13 14:00:37 +0100

    changed training

--- examples/dec_mnist.py
+++ examples/dec_mnist.py
@@ -96,17 +96,31 @@ def main(cuda, gpu_id, batch_size, pretrain_epochs, finetune_epochs, testing_mod
         autoencoder.load_state_dict(torch.load(path_to_out/'pretrain_ae'))
     else:
         print("Pretraining stage.")
-        ae.pretrain(
+        # ae.pretrain(
+        #     ds_train,
+        #     autoencoder,
+        #     #loss_fn=partial(SobelLoss, torch.nn.MSELoss, True, cuda),
+        #     cuda=cuda,
+        #     validation=ds_val,
+        #     epochs=pretrain_epochs,
+        #     batch_size=batch_size,
+        #     optimizer=lambda model: SGD(model.parameters(), lr=0.1, momentum=0.9),
+        #     scheduler=lambda x: StepLR(x, 100, gamma=0.1),
+        #     corruption=0.2,
+        # )
+        ae_optimizer = SGD(params=autoencoder.parameters(), lr=0.1, momentum=0.9)
+        ae.train(
             ds_train,
             autoencoder,
-            #loss_fn=partial(SobelLoss, torch.nn.MSELoss, True, cuda),
+            loss_fn=partial(SobelLoss, torch.nn.MSELoss, True, cuda),
             cuda=cuda,
             validation=ds_val,
-            epochs=pretrain_epochs,
+            epochs=finetune_epochs,
             batch_size=batch_size,
-            optimizer=lambda model: SGD(model.parameters(), lr=0.1, momentum=0.9),
-            scheduler=lambda x: StepLR(x, 100, gamma=0.1),
-            corruption=0.2,
+            optimizer=ae_optimizer,
+            scheduler=StepLR(ae_optimizer, 100, gamma=0.1),
+            corruption=0.5,
+            update_callback=training_callback,
         )
         torch.save(autoencoder.state_dict(), path_to_out/'pretrain_ae')
     print('Saving features after pretraining.')
