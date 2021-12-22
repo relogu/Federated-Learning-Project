025a22e7b2d6570b3cdbc2243b878f696d9f97a0
relogu <lollonasi97@gmail.com>
2021-12-22 12:20:29 +0100

    correctted operrations and added unflatten

--- py/dec/layers/torch/truncated_gaussian_noise.py
+++ py/dec/layers/torch/truncated_gaussian_noise.py
@@ -4,30 +4,32 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 
 class TruncatedGaussianNoise(nn.Module):
-    def __init__(self, shape, stddev: float = 0.05, rate: float = 0.5, seed=None, cuda: bool = False):
+    def __init__(self, shape, stddev: float = 0.05, rate: float = 0.5, unflatten: bool = False, seed=None, cuda: bool = False):
         super(TruncatedGaussianNoise, self).__init__()
         self.stddev = stddev
         self.rate = rate
         self.num_samples = int(self.rate*shape)
         self.seed = seed
         self.shape = shape
+        self.unflatten = unflatten
         self.cuda = cuda
         if self.cuda:
             self.noise = Variable(torch.zeros(self.shape,self.shape).cuda())
         else:
             self.noise = Variable(torch.zeros(self.shape,self.shape))
-        if self.cuda:
-            self.rates = torch.ones(self.shape,self.shape).cuda()
-        else:
-            self.rates = torch.ones(self.shape,self.shape)
 
     def forward(self, input):
-        if not self.training: return input
-        self.noise.data.normal_(0, std=self.stddev)
-        if self.cuda:
-            mask = (torch.rand(self.shape) > self.rate).cuda()
-        else:
-            mask = torch.rand(self.shape) > self.rate
-        self.noise = self.noise.masked_fill(mask, 0)
-        return input + self.noise
+        if self.unflatten:
+            input = nn.Unflatten(1, (1, 28, 28))(input)
+        if self.training:
+            self.noise.data.normal_(0, std=self.stddev)
+            if self.cuda:
+                mask = (torch.rand(self.shape) > self.rate).cuda()
+            else:
+                mask = torch.rand(self.shape) > self.rate
+            self.noise = self.noise.masked_fill(mask, 0)
+            input = input + self.noise
+        if self.unflatten:
+            input = nn.Flatten()(input)
+        return input
     
