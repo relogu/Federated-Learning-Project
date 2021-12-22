ed19fcf0d05273b236991623c87fc03618c13f2b
relogu <lollonasi97@gmail.com>
2021-12-22 12:23:44 +0100

    removed useless unfalttening

--- py/dec/layers/torch/truncated_gaussian_noise.py
+++ py/dec/layers/torch/truncated_gaussian_noise.py
@@ -4,14 +4,13 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 
 class TruncatedGaussianNoise(nn.Module):
-    def __init__(self, shape, stddev: float = 0.05, rate: float = 0.5, unflatten: bool = False, seed=None, cuda: bool = False):
+    def __init__(self, shape, stddev: float = 0.05, rate: float = 0.5, seed=None, cuda: bool = False):
         super(TruncatedGaussianNoise, self).__init__()
         self.stddev = stddev
         self.rate = rate
         self.num_samples = int(self.rate*shape)
         self.seed = seed
         self.shape = shape
-        self.unflatten = unflatten
         self.cuda = cuda
         if self.cuda:
             self.noise = Variable(torch.zeros(self.shape,self.shape).cuda())
@@ -19,8 +18,6 @@ class TruncatedGaussianNoise(nn.Module):
             self.noise = Variable(torch.zeros(self.shape,self.shape))
 
     def forward(self, input):
-        if self.unflatten:
-            input = nn.Unflatten(1, (1, 28, 28))(input)
         if self.training:
             self.noise.data.normal_(0, std=self.stddev)
             if self.cuda:
@@ -29,7 +26,5 @@ class TruncatedGaussianNoise(nn.Module):
                 mask = torch.rand(self.shape) > self.rate
             self.noise = self.noise.masked_fill(mask, 0)
             input = input + self.noise
-        if self.unflatten:
-            input = nn.Flatten()(input)
         return input
     
