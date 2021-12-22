ce15a396e00343b83079324a6af856f91e95b7db
relogu <lollonasi97@gmail.com>
2021-12-22 12:11:16 +0100

    added noising layer for prob data

---
+++ py/dec/layers/torch/truncated_gaussian_noise.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+class TruncatedGaussianNoise(nn.Module):
+    def __init__(self, shape, stddev: float = 0.05, rate: float = 0.5, seed=None, cuda: bool = False):
+        super(TruncatedGaussianNoise, self).__init__()
+        self.stddev = stddev
+        self.rate = rate
+        self.num_samples = int(self.rate*shape)
+        self.seed = seed
+        self.shape = shape
+        self.cuda = cuda
+        if self.cuda:
+            self.noise = Variable(torch.zeros(self.shape,self.shape).cuda())
+        else:
+            self.noise = Variable(torch.zeros(self.shape,self.shape))
+        if self.cuda:
+            self.rates = torch.ones(self.shape,self.shape).cuda()
+        else:
+            self.rates = torch.ones(self.shape,self.shape)
+
+    def forward(self, input):
+        if not self.training: return input
+        self.noise.data.normal_(0, std=self.std)
+        if self.cuda:
+            mask = (torch.rand(self.shape) > self.rate).cuda()
+        else:
+            mask = torch.rand(self.shape) > self.rate
+        self.noise = self.noise.masked_fill(mask, 0)
+        return input + self.noise
+    
