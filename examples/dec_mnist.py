170953244ca487258f46780c277c5bd6636f059b
relogu <lollonasi97@gmail.com>
2022-01-06 13:43:54 +0100

    chnaged ae opt

--- examples/dec_mnist.py
+++ examples/dec_mnist.py
@@ -229,13 +229,14 @@ def main(cuda, gpu_id, batch_size, pretrain_epochs, finetune_epochs, testing_mod
             )
         else:
             # pretraining with standard methods (may be apply some kind of noise to data that is not d/o)
-            lambda_ae_opt = lambda model: Yogi(
-                model.parameters(),
-                lr=adam_lr,
-                betas=(0.9, 0.999),
-                eps=1e-3,
-                initial_accumulator=1e-6,
-                weight_decay=0)  # Adam(model.parameters(), lr=adam_lr)
+            # lambda_ae_opt = lambda model: Yogi(
+            #     model.parameters(),
+            #     lr=adam_lr,
+            #     betas=(0.9, 0.999),
+            #     eps=1e-3,
+            #     initial_accumulator=1e-6,
+            #     weight_decay=0)  
+            lambda_ae_opt = lambda model: Adam(model.parameters(), lr=adam_lr)
             lambda_scheduler = lambda x: ReduceLROnPlateau(
                 x,
                 mode='min',
@@ -309,13 +310,14 @@ def main(cuda, gpu_id, batch_size, pretrain_epochs, finetune_epochs, testing_mod
             ae_opt = SGD(autoencoder.parameters(), lr=0.1, momentum=0.9)
             scheduler = StepLR(ae_opt, 100, gamma=0.1)
         else:
-            ae_opt = Yogi(
-                autoencoder.parameters(),
-                lr=adam_lr,
-                betas=(0.9, 0.999),
-                eps=1e-3,
-                initial_accumulator=1e-6,
-                weight_decay=0)  # Adam(model.parameters(), lr=adam_lr)
+            # ae_opt = Yogi(
+            #     autoencoder.parameters(),
+            #     lr=adam_lr,
+            #     betas=(0.9, 0.999),
+            #     eps=1e-3,
+            #     initial_accumulator=1e-6,
+            #     weight_decay=0)
+            ae_opt = Adam(autoencoder.parameters(), lr=adam_lr)
             scheduler = ReduceLROnPlateau(
                 ae_opt,
                 mode='min',
