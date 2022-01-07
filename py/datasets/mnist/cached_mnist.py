9e05793e526fb3a38d8bd66b6c758f2793e4d889
relogu <lollonasi97@gmail.com>
2022-01-07 12:26:18 +0100

    changed device managment

--- py/datasets/mnist/cached_mnist.py
+++ py/datasets/mnist/cached_mnist.py
@@ -4,10 +4,10 @@ from torchvision import transforms
 from torchvision.datasets import MNIST
 
 class CachedMNIST(Dataset):
-    def __init__(self, train, cuda, testing_mode=False):
+    def __init__(self, train, device, testing_mode=False):
         img_transform = transforms.Compose([transforms.Lambda(self._transformation)])
         self.ds = MNIST("./data", download=True, train=train, transform=img_transform)
-        self.cuda = cuda
+        self.device = device
         self.testing_mode = testing_mode
         self._cache = dict()
 
@@ -20,11 +20,11 @@ class CachedMNIST(Dataset):
     def __getitem__(self, index: int) -> torch.Tensor:
         if index not in self._cache:
             self._cache[index] = list(self.ds[index])
-            if self.cuda:
-                self._cache[index][0] = self._cache[index][0].cuda(non_blocking=True)
-                self._cache[index][1] = torch.tensor(self._cache[index][1]).cuda(
-                    non_blocking=True
-                )
+            self._cache[index][0] = self._cache[index][0].to(self.device, non_blocking=True)
+            self._cache[index][1] = torch.tensor(self._cache[index][1]).to(
+                self.device,
+                non_blocking=True
+            )
         return self._cache[index]
 
     def __len__(self) -> int:
