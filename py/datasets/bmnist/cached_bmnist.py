6549a75fd13abda2179f6f5f4589650f09bb718d
relogu <lollonasi97@gmail.com>
2022-02-19 11:59:46 +0100

    corrected device management

--- py/datasets/bmnist/cached_bmnist.py
+++ py/datasets/bmnist/cached_bmnist.py
@@ -23,11 +23,11 @@ class CachedBMNIST(Dataset):
     def __getitem__(self, index: int) -> torch.Tensor:
         if index not in self._cache:
             self._cache[index] = list(self.ds[index])
-            if self.cuda:
-                self._cache[index][0] = self._cache[index][0].cuda(non_blocking=True)
-                self._cache[index][1] = torch.tensor(self._cache[index][1]).cuda(
-                    non_blocking=True
-                )
+            self._cache[index][0] = self._cache[index][0].to(self.device, non_blocking=True)
+            self._cache[index][1] = torch.tensor(self._cache[index][1]).to(
+                self.device,
+                non_blocking=True
+            )
         return self._cache[index]
 
     def __len__(self) -> int:
